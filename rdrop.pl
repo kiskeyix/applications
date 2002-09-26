29391eecfca60038b2b15ee5242db75248922c4e
Luis Mondesi <lemsx1@gmail.com>
2002-09-26 15:21:04 +0000

    a Perl script for droping files to be sync'ed to a remote server

---
+++ rdrop.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+# $Id: rdrop.pl,v 1.1 2002-09-26 15:21:04 luigi Exp $
+# Last modified: 2002-Sep-26
+# 
+# Luis Mondesi < lemsx1@hotmail.com >
+#
+# This script uses GTK to create a GUI to allow users to drop files
+# on top of them and rsync's them over SSH to a remote server.
+#
+use strict;
+$|++;
+
+

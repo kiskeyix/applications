e0873fb6acf4989ac9cfe10d26890b4dd53d6b16
Luis Mondesi <lemsx1@gmail.com>
2002-11-01 16:23:12 +0000

    *** empty log message ***

--- perms.sh
+++ perms.sh
@@ -1,6 +1,6 @@
 #!bash
-# $Id: perms.sh,v 1.3 2002-11-01 16:22:54 luigi Exp $
-# $R: $
+# $Id: perms.sh,v 1.4 2002-11-01 16:23:12 luigi Exp $
+# $V: $
 # Luis Mondesi < lemsx1@hotmail.com >
 # Last modified: 2002-Nov-01
 #

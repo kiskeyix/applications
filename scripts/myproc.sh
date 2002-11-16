fd8adc05e3a9cf6a2e2e867b44c7d753200c9510
Luis Mondesi <lemsx1@gmail.com>
2002-11-16 18:05:43 +0000

    minor mod. now I use the same listing for Solaris and Linux

--- scripts/myproc.sh
+++ scripts/myproc.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-ps ax | grep -v myproc | grep -i $1 
+# Last modified: 2002-Nov-16
+# Luis Mondesi < lemsx1@hotmail.com >
+# 
+# need this command too much...
+ps -ef | grep -v myproc | grep -v grep | grep -i $1 

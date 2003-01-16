d7f3f71a2e4e0530ba8c0a6a75a4fa4a5391acd8
Luis Mondesi <lemsx1@gmail.com>
2003-01-16 22:36:11 +0000

    new improved version

--- scripts/pixdir2html.pl
+++ scripts/pixdir2html.pl
@@ -1,12 +1,25 @@
 #!/usr/bin/perl 
-# do not show so many warnings: -w ;    ;-)
-
+# $Revision: 1.2 $
 # Luis Mondesi  <lemsx1@hotmail.com> 2002-01-17
-# Use this script in Nautilus to create HTML files
+# 
+# USAGE:
+#   pictDir2html.pl [DIR] [force] [nomenu]
+# 
+#   DIR     - make thumbnails for DIR or current directory if no argument
+#             is given
+#   force   - creates a .pictDir2htmlrc in every subdir overriding
+#             any file with that name
+#   nomenu  - do not create menus after finishing creating thumbnails
+#   
+# DESCRIPTION:
+# 
+# Use this non-interactive script in Nautilus to create HTML files
 # with their proper thumbnails for pictures (.jpeg or .gif)
 # 
 # Make this file executable and put it in:
 # ~/.gnome/nautilus-scripts
+# or 
+# ~/.gnome2/nautilus-scripts
 # 
 # Then run it from from the File::Scripts::script_name menu
 # 
@@ -28,195 +41,310 @@
 # 
 # These are the only tags that you can customize for now :-)
 # Required: linux/UNIX "convert" command (to convert images from
-#	    one format to another or sizes, etc...
+#	    one format to another or sizes, etc...)
 # 
-# Known bugs: config file cannot contain lines that spawn to multiple
-# lines. Will fix later.
+# BUGS:
+#   * config file cannot contain lines that spawn to multiple
+#     lines. Will fix later.
+#   * config file should not contain double quotes (") without
+#     escaping them first (\")
+#   * if a directory has a .nopictDir2htmlrc file, it will be skipped
+#     even if there are subdirectories inside which might need thumbnails
+#     created. Either do not put a .nopictDir2htmlrc file in these directories
+#     or mv the subdirectories parallel to this directory. Will deal with
+#     this issue later when menuMaker subroutine is completed and completely
+#     recursive
+# 
+# TIPS:
 # 
 # Put a .nopictDir2htmlrc file in directories for which you do not want
 # thumbnails and/or index.html to be written
 #
-#use Image::Size; #messures image sizes... not needed for now
+use File::Copy;
 use strict;
 use vars qw( $VERSION @INC );
 use Config;
 my $VERSION="0.3";
 $|++; # disable buffer
 
-# later I will use $argument = shift; to parse all directories
-# for now, only one dir can be parse at a time :-D
-
-# Use current directory works with Nautilus :-)
-# put this file in ~/.gnome/nautilus-scripts
-my $IMAGE_DIRECTORY=(-d "$ARGV[0]") ? "$ARGV[0]":".";
+# update these if needed
+my $HTML_DIRECTORY=".";
+my $LOG="$HTML_DIRECTORY/pictDir2html.log";
 my $FILE_NAME="index.php";
 my $CONFIG_FILE=".pictDir2htmlrc";
 my $THUMBNAIL="t";
+
+# list directories that should be skipped here
+# separated by |
+my $EXCEPTION_LIST = "CVS|RCS";
 
 # How big are the thumbnails?
 # This is the default, in case the config file
 # doesn't exist or do not have this item in it
 my $PERCENT="20%";
 # How many TDs per table?
-my $tds=4;
-# Not implemented yet:
-# How many TRs before going to the next page?
-# my $trs=4;
+my $td=4;
+
+#TODO will make this into  a sub later
+#       remember to make it recursive
+my $MENUMAKER=$ENV{"HOME"}."/bin/menuMaker.pl";
+
+# dont worry if you don't have a log rotation facility...
+# just leave it as is
+my $SAVELOG = "/usr/bin/savelog";
 
 ###Nothing below this line should need to be configured.###
-my $line = "";
-my $thisFile= "";
-my $x=0;
-my $y=0;
-my $i=0;
-my $total_picts=0;
-my @ls = ();
-my @ts = ();
-my %myconfig = ();
+#**************************************************************#
+
+my $IMAGE_DIRECTORY=(-d "$ARGV[0]") ? "$ARGV[0]":".";
+my $FORCE=(($ARGV[1] =~ m/force/i) || ($ARGV[2] =~ m/force/i) ) ? 1 : 0; 
+my $NOMENU=(($ARGV[1] =~ m/nomenu/i) || ($ARGV[2] =~ m/nomenu/i) ) ? 1 : 0; 
+
+my $total_directories=0;
 my $THUMBNAILSDIR="$IMAGE_DIRECTORY/$THUMBNAIL";
 
 warn << "__EOF__";
 Perl PictDir2HTML v$VERSION (Luis Mondesi <lemsx1\@hotmail.com> / LatinoMixed.com) (running with Perl $] on $Config{'archname'}) \n \n
 __EOF__
 
-# test for .nopictDir2htmlrc file
-open(NOCONFIG,"$IMAGE_DIRECTORY/.nopictDir2htmlrc") && die ".nopictDir2htmlrc file exists here ($IMAGE_DIRECTORY). Exiting...";
-close(NOCONFIG);
+main();
+
+#-------------------------------------------------#
+#                     FUNCTIONS                   #
+#-------------------------------------------------#
+sub main {
+    if (!-x "/usr/bin/convert") {
+        die ("could not find 'convert'. Install ImageMagick.");
+    }
+    open (LOGFILE,"> $LOG");
+    mkthumb($IMAGE_DIRECTORY);
+    unless ( $NOMENU == 1 ) {
+        print LOGFILE ("Creating menu file\n");
+        system("$MENUMAKER >> $LOG 2>&1 ");
+    }
+    close(LOGFILE);
+    if ( -x $SAVELOG ) {
+        system("$SAVELOG $LOG > /dev/null 2>&1");
+    }
+
+    print STDOUT "$total_directories directories.\n Read log $LOG for details. \n";
+} # endmain
+
+# Takes one argument directory to create images for
+# If a directory is found inside this directory containing
+# images, then it recursively calls itself over and over
+sub mkthumb {
+    
+    my $ROOT = $_[0];
+    $THUMBNAILSDIR="$ROOT/$THUMBNAIL";
+
+    my @subdir = ();
+    my @ls = ();
+    my @ts = ();
+    my %myconfig = ();
+
+    my $line = "";
+    my $thisFile= "";
+    my $i=0;
+    my $total_picts=0;
+    
+    opendir (DIR,"$ROOT") || die "Couldn't open dir $ROOT";
+    
+    print LOGFILE ("Working in $ROOT \n");
+    
+#construct array of all image files
+    while (defined($thisFile = readdir(DIR))) {
+        next if ($thisFile =~ m/$EXCEPTION_LIST/);
+        next if ($thisFile !~ /\w/);
+        next if ($thisFile =~ /^\..*/); 
+        if (-d "$ROOT/$thisFile" && $thisFile !~ m/^$THUMBNAIL$/ ) {
+            if (-f "$ROOT/$thisFile/.nopictDir2htmlrc") {
+                print LOGFILE ".nopictDir2htmlrc file exists in ($thisFile). Skipping ...\n";
+                next;
+            }
+            $total_directories++;
+            push @subdir,"$ROOT/$thisFile";
+   
+            if (-f "$ROOT/$thisFile/$CONFIG_FILE") {
+                if ($FORCE == 1) {
+                    if ( 
+                        copy("$HTML_DIRECTORY/$CONFIG_FILE", 
+                            "$ROOT/$thisFile/$CONFIG_FILE") 
+                    ) {
+                        print LOGFILE ("force copied $HTML_DIRECTORY/$CONFIG_FILE \
+                            to $ROOT/$thisFile/$CONFIG_FILE \n");
+                    }
+                } 
+            } else {
+                #file doesn't exist, copy it
+                if (
+                copy("$HTML_DIRECTORY/$CONFIG_FILE", 
+                    "$ROOT/$thisFile/$CONFIG_FILE")
+                ) {
+                    print LOGFILE ("copied $CONFIG_FILE to \
+                        $ROOT/$thisFile/$CONFIG_FILE \n");
+                }
+            }
+        }
+        next if ($thisFile !~ m/\.(jpg|png|jpeg|gif)/i);
+        push @ls,$thisFile;
+        $total_picts++;
+    } #end images array creation
+    closedir(DIR);
 
 #do we already have a dir with this name? no, then create one
-opendir(TESTDIR,"$THUMBNAILSDIR") || mkdir($THUMBNAILSDIR,0755);
-#close it now (regardless
-closedir(TESTDIR);
-
-opendir (DIR,"$IMAGE_DIRECTORY") || die "Couldn't open dir $IMAGE_DIRECTORY";
-#construct array of all image files
-while (defined($thisFile = readdir(DIR))) {
-    next if (-d "$IMAGE_DIRECTORY/$thisFile");
-    next if ($thisFile !~ /\w/);
-    next if ($thisFile !~ m/\.(jpg|png|jpeg|gif)/i);
-    $ls[$x] = $thisFile;
-    $x+=1;
-}
-closedir(DIR);
-
-$total_picts = $x;
-
-open(FILE, "> $IMAGE_DIRECTORY/$FILE_NAME") || die "Couldn't write file $FILE_NAME to $IMAGE_DIRECTORY";
-if (open(CONFIG, "<$IMAGE_DIRECTORY/$CONFIG_FILE")){
-    while (<CONFIG>) {
-	next if /^\s*#/;
-        chomp;
-        $myconfig{$1} = $2 if /^\s*([^=]+)=(.+)\;\;+/;
+    if (!-d "$THUMBNAILSDIR" && !-f "$ROOT/.nopictDir2htmlrc") { 
+        print LOGFILE ("making thumbnails directory in $THUMBNAILSDIR\n");
+        mkdir("$THUMBNAILSDIR",0755);
     }
-    close(CONFIG);
 
-} else {
-    warn << "__EOF__";
-   Could not find $IMAGE_DIRECTORY/$CONFIG_FILE 
+    if (!-f "$ROOT/.nopictDir2htmlrc") {
+        open(FILE, "> $ROOT/$FILE_NAME") || die "Couldn't write file $FILE_NAME to $ROOT";
+        if (open(CONFIG, "<$ROOT/$CONFIG_FILE")){
+            while (<CONFIG>) {
+                next if /^\s*#/;
+                chomp;
+                $myconfig{$1} = $2 if m/^\s*([^=]+)=(.+)\;\;+/;
+            }
+            close(CONFIG);
+
+        } else {
+            warn << "__EOF__";
+   Could not find $ROOT/$CONFIG_FILE 
 __EOF__
 
-    $myconfig{percent}="20%";
-    $myconfig{title}="Images";
-    $myconfig{meta}="<meta http-equiv='content-type' content='text/html;charset=iso-8859-1'>";
-    $myconfig{stylesheet}="<link rel='stylesheet' href='../styles.css' type='text/css'>";
-    $myconfig{html_msg}="<h1>Free form HTML</h1>";
-    $myconfig{body}="<body bgcolor='#000000' text='#ffffff'>";
-    $myconfig{p}="<p>";
-    $myconfig{table}="<table border='0'>";
-    $myconfig{td}="<td valign='top' align='left'>";
-    $myconfig{tr}="<tr>";
-    $myconfig{footer}="";
+            $myconfig{percent}="20%";
+            $myconfig{title}="Images";
+            $myconfig{meta}="<meta http-equiv='content-type' content='text/html;charset=iso-8859-1'>";
+            $myconfig{stylesheet}="<link rel='stylesheet' href='../styles.css' type='text/css'>";
+            $myconfig{html_msg}="<h1>Free form HTML</h1>";
+            $myconfig{body}="<body bgcolor='#000000' text='#ffffff'>";
+            $myconfig{p}="<p>";
+            $myconfig{table}="<table border='0'>";
+            $myconfig{td}="<td valign='top' align='left'>";
+            $myconfig{tr}="<tr>";
+            $myconfig{footer}="";
 
-}
+        }
 #construct a header if it doesn't yet exist:
-if ( $myconfig{header} eq "" ) {
-    
-    print "Blank header. Generating my own ... \n";
-    
-    $myconfig{header}="<html>
-    <head>
-	".$myconfig{meta}."
-	<title>".$myconfig{title}."</title>
-	".$myconfig{stylesheet}."
-    </head>".
-    $myconfig{body}."
-    <center>".
-    $myconfig{html_msg}."
-    \n
-    ";
-}
+        if ( $myconfig{header} eq "" ) {
+
+            print LOGFILE ("Blank header. Generating my own ... \n");
+
+            $myconfig{header}="<html>
+            <head>
+            ".$myconfig{meta}."
+            <title>".$myconfig{title}."</title>
+            ".$myconfig{stylesheet}."
+            </head>".
+            $myconfig{body}."
+            <center>".
+            $myconfig{html_msg}."
+            \n
+            ";
+        }
 
 # Percentage for this folder?
-$PERCENT = ("$myconfig{percent}") ? $myconfig{percent}:$PERCENT;
+        $PERCENT = ("$myconfig{percent}") ? $myconfig{percent}:$PERCENT;
 
 #print contents of array @ls
 
 # start HTML
-print FILE ("$myconfig{header}");
+        print FILE ("$myconfig{header}\n");
 
 # start table
-print FILE ("$myconfig{table}");
+        print FILE ("$myconfig{table}\n");
 
-my $my_bgcolor = "";
+        my $my_bgcolor = "";
 
 #print all picts now
-while($x > 0){
-    if ( $myconfig{tr} =~ m/\%+bgcolor\%+/i ) {
-        #print STDERR "yess, it did: $x \n ";
-        # make sure modulus is calculated right
-        #if ( ($x % 2) == 0 ){
-            #print STDERR "modulus used";
-            #($myconfig{tr} = $myconfig{tr}) =~ s/\%+bgcolor\%+/bgcolor=#cccccc/i;
-            #} else {
-            #print STDERR "No modulus";
-             ($myconfig{tr} = $myconfig{tr}) =~ s/\%+bgcolor\%+//i;
-             #}
-    }
-    print FILE ($myconfig{tr}."\n");
-    for ($y=1;$y<=$tds;$y++){
-        #if (open (Test,"$THUMBNAILSDIR/t"."$ls[$i]")){
-        if ( -f "$THUMBNAILSDIR/t"."$ls[$i]" ){
-	    # file exists, no need to create
-	    # if not then create thumbnail below
-	}else{
-	if ( -f "$IMAGE_DIRECTORY/$ls[$i]" ){
-            if (!-x "/usr/bin/convert") {
-                die ("could not find 'convert'");
+        foreach(@ls){
+            if ( !-f "$THUMBNAILSDIR/"."t$_" ){
+                # TODO determine which thumbnails need to be recreated
+                # $FORCE_ALL || !-f "$THUMBNAILSDIR/"."t$_" 
+                # file doesn't exists or we are being forced to recreated
+                # create thumbnail below
+                if ( -f "$ROOT/$_" ){
+
+                    print LOGFILE ("\nConverting file $ROOT/$_ into $THUMBNAILSDIR/t$_ \n");
+                    system("convert -geometry $PERCENT $ROOT/$_ $THUMBNAILSDIR/"."t$_");
+                    print LOGFILE ("\n"); 
+                } # end if -f ROOT/ls[i]
+            } # end if THUMBNAILSDIR/myfile
+
+            if ($i == 0) {
+                # open a new row
+                if ( $myconfig{tr} =~ m/\%+bgcolor\%+/i ) {
+                    ($myconfig{tr} = $myconfig{tr}) =~ s/\%+bgcolor\%+//i;
+                }
+
+                print FILE ($myconfig{tr}."\n");
+            } 
+            print FILE ("\t".$myconfig{td}."\n");
+            if (-f "$THUMBNAILSDIR/"."t$_"){
+                # if file exists, create a link, otherwise leave it blank
+                print FILE ("<a href='$_'><img src='$THUMBNAIL/"."t$_'></a>\n");
+            } else {
+                print FILE ("&nbsp;");
             }
-	    print ("\nConverting file $IMAGE_DIRECTORY/$ls[$i] into $THUMBNAILSDIR/t$ls[$i] \n");
-	    system("convert -geometry $PERCENT $IMAGE_DIRECTORY/$ls[$i] $THUMBNAILSDIR/t"."$ls[$i]");
-	   print ("\n"); 
-	    } # end if -f IMAGE_DIRECTORY/ls[i]
-	} # end if/else open Test
-	print FILE ("\t".$myconfig{td}."\n");
-	if (open Test,"$THUMBNAILSDIR/t"."$ls[$i]"){
-	    # if file exists, create a link, otherwise leave it blank
-	    print FILE ("<a href='$ls[$i]'><img src='$THUMBNAIL/t"."$ls[$i]'></a>\n");
-	} else {
-	    print FILE ("&nbsp;");
-	}
-	print FILE ("\t</td>\n");
-	$i++;
-	$x--;
-    }#end for $y
-    print FILE ("</tr>\n");
-}
-print FILE ("</table>\n");
+            print FILE ("\t</td>\n");
+            if ($i<($td-1)) {
+                $i++;
+            } else {
+                # wrap and reset counter
+                print FILE ("</tr>\n");
+                $i = 0;
+            }
+        } #end foreach
+        # complete missing TD
+        if ($i != 0) {
+            for (;$i<$td;$i++) {
+                print FILE ("\t".$myconfig{td}."\n");
+                print FILE ("&nbsp;");
+                print FILE ("\t</td>\n");
+            }
+        }
+        print FILE ("</tr>\n");
+        print FILE ("</table>\n");
 
 # close the footer if one doesn't exist:
-if ( $myconfig{footer} eq "" ) {
-    print FILE ($myconfig{footer}."\n");
-    print FILE ("</center></body>\n");
-    print FILE ("</HTML>");
-} else {
+        if ( $myconfig{footer} eq "" ) {
+            print FILE ($myconfig{footer}."\n");
+            print FILE ("</center></body>\n");
+            print FILE ("</HTML>");
+        } else {
 
-    print FILE ($myconfig{footer});
-}
-close(FILE);
-print STDERR "Done. I count $total_picts pictures here $IMAGE_DIRECTORY.\n";
+            print FILE ($myconfig{footer});
+        }
+        close(FILE);
+        print LOGFILE "Counted $total_picts pictures here $ROOT\n";
+    }
 
-#sub tumb {
+# loop thru rest of directories
+    foreach(@subdir){
+        mkthumb("$_"); 
+    }
+} #end mkthumb
+
+#sub thumbfile {
     # creates an HTML page for a thumbnail
     # this will be implemented later
 
     #}
+sub prompt {
+    # promt user and return input 
+    # pass string when calling subroutine: $var = prompt("string");
+    my($string) = $_[0];#shift;
+    my($input) = "";
+    
+        #if ($Suppress_readline) { 
+    print ("* ".$string."\n");
+    chomp($input = <STDIN>);
+        # chomp is the same as:
+        # $input =~ s/\n//g; # remove lineend
+        #} else {
+        # No readline support for now
+        #$input = $term->readline($string);
+        #}
+    return $input;
+}

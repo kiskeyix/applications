296b27378b0b26d4cffa01a4b0e3023ce00b8fb5
Luis Mondesi <lemsx1@gmail.com>
2003-01-31 17:42:06 +0000

    cleanup email messages

--- scripts/webcam.sh
+++ scripts/webcam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2003-Jan-30
+# Last modified: 2003-Jan-31
 #
 # DESCRIPTION: a script to start|stop "webcam"
 # USAGE: webcam.sh [start|stop|restart] 
@@ -59,7 +59,7 @@ set -e
 case "$1" in
     start)
 
-    if test -f $PID; then
+    if [ -f $PID ]; then
         MESSAGE="PID file $PID exists. This usually mean that $DESC is running. Stop $DESC before starting a new one or remove the file if this is an error."
         echo $MESSAGE
 
@@ -69,67 +69,81 @@ case "$1" in
         exit 1
     fi
 
-	echo -n "Starting $DESC: "
-        /sbin/start-stop-daemon --background $CHUID --start \
-            --make-pidfile --pidfile $PID \
-            --verbose --exec $DAEMON $CMDLINE || echo -n "<Failed> "
-        echo -n "$NAME"
-        if [ $EMAILUSER = "yes" -a -f $PID ]; then
+    # create default directory if not there
+    if [ ! -d $PHOTO ]; then
+        mkdir $PHOTO
+    fi
+
+    echo -n "Starting $DESC: "
+
+    /sbin/start-stop-daemon --background $CHUID --start \
+        --make-pidfile --pidfile $PID \
+        --verbose --exec $DAEMON $CMDLINE || echo -n "<Failed> "
+
+    echo -n "$NAME"
+
+    if [  -f $PID ]; then
+        if [ $EMAILUSER = "yes" ]; then
             MESSAGE="$DESC succesfully started"
             echo $MESSAGE | $EMAILBIN -s "$DESC success" $EMAILADDRESS $USER
-        else
+        fi
+    else
+        if [ $EMAILUSER = "yes" ]; then
             MESSAGE="$DESC startup failed"
             echo $MESSAGE | $EMAILBIN -s "$DESC error" $EMAILADDRESS $USER
-
         fi
-        ;;
+    fi
+    ;;
 
     stop)
-        echo -n "Stopping $DESC: "
-        # --exec is needed when no PID/pidfile is given
-        /sbin/start-stop-daemon --stop --verbose \
-            --pidfile $PID --exec $REALDAEMON || echo -n "<Failed> "
-        echo -n "$NAME"
-        rm -f $PID
-        
-        if [ $EMAILUSER = "yes" -a ! -f $PID ]; then
+    echo -n "Stopping $DESC: "
+    # --exec is needed when no PID/pidfile is given
+    /sbin/start-stop-daemon --stop --verbose \
+        --pidfile $PID --exec $REALDAEMON || echo -n "<Failed> "
+    echo -n "$NAME"
+    rm -f $PID
+
+    if [ ! -f $PID ]; then
+        if [ $EMAILUSER = "yes" ]; then
             MESSAGE="$DESC succesfully stopped"
             echo $MESSAGE | $EMAILBIN -s "$DESC stopped" $EMAILADDRESS $USER
-        else
+        fi
+    else
+        if [ $EMAILUSER = "yes" ]; then
             MESSAGE="$DESC stopping failed"
             echo $MESSAGE | $EMAILBIN -s "$DESC error" $EMAILADDRESS $USER
-
         fi
+    fi
 
-        # rename directory and create a new empty directory
-        $MV $PHOTO $PHOTO-$APPENDTONAME.$$$$ && mkdir $PHOTO
+    # rename directory and create a new empty directory
+    $MV $PHOTO $PHOTO-$APPENDTONAME.$$$$ && mkdir $PHOTO
 
-        ;;
+    ;;
     restart)
         echo -n "Restarting $DESC: "
         $0 stop
         sleep 5
         $0 start
-        ;;
+    ;;
     status)
-        if test -f $PID; then
-            ps ax | grep -v grep | grep -i $NAME
-        else
-            echo "$DESC is not running."
+    if test -f $PID; then
+        ps ax | grep -v grep | grep -i $NAME
+    else
+        echo "$DESC is not running."
 
-        fi
-        ;;
+    fi
+    ;;
 
-  *)
-	echo "Usage: $0 {start|stop|restart}" >&2
-	exit 1
-	;;
+    *)
+    echo "Usage: $0 {start|stop|restart}" >&2
+    exit 1
+    ;;
 esac
 
 if [ $? == 0 ]; then
-	echo .
-	exit 0
+    echo .
+    exit 0
 else
-	echo failed
-	exit 1
+    echo failed
+    exit 1
 fi

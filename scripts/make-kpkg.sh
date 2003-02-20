430ac14ae9cecbf013cd20de0d766b9cc0be10d9
Luis Mondesi <lemsx1@gmail.com>
2003-02-20 04:09:09 +0000

    using fakeroot and now packages can be built on any directory! not just /usr/src/linux

--- scripts/make-kpkg.sh
+++ scripts/make-kpkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2003-Feb-07
+# Last modified: 2003-Feb-09
 #
 # DESCRIPTION: Use the wonderful "make-kpkg" from Debian
 #               to build a custom kernel.
@@ -15,10 +15,18 @@
 # CHANGELOG:
 #
 
+FAKEROOT=fakeroot
+
+MODULE_LOC="../modules/" # modules are located in the directory prior to this
+PATCH_THE_KERNEL="YES" # always patch the kernel
+ALL_PATCH_DIR="../kernel-patches/" # patches are located before this dir
+IMAGE_TOP="../" # where to save the resulting .deb files
+
+export IMAGE_TOP ALL_PATCH_DIR PATCH_THE_KERNEL MODULE_LOC
 if [ $1 ]; then
     echo -e "Building kernel"
     make-kpkg clean
-    make-kpkg --initrd --config oldconfig --append-to-version -custom.$1 --revision $1 clean modules_clean kernel_image modules_image
+    make-kpkg --rootcmd $FAKEROOT  --initrd --config oldconfig --append-to-version -custom.$1 --revision $1 clean modules_clean kernel_image modules_image
 else
     echo -e "Usage: $0 ## \n \t Where ## is an interger"
 fi

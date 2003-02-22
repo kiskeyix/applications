08de7e7d5e49fd884e15457e205ad358d680ae54
Luis Mondesi <lemsx1@gmail.com>
2003-02-22 04:47:26 +0000

    found that only grep 2.5.x supports -m

--- temp.sh
+++ temp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 # 
 # DESC: close program given by "list" if temperature
 #       is higher than "limit"
@@ -10,11 +10,14 @@ LIMIT=50
 # start killing these programs (separate by spaces)
 LIST="setiathome"
 
+# 
 # command to cleanup the temperature
 # it gets the CPU line out of the output
 # of "sensors" and then cleans it up
-# -m ("max line matches") -i ("insensitive")
-TEMP=`sensors | grep -m 1 -i "CPU temp" | cut -d" " -f4 | sed "s/\..*//g" | sed "s/[\+\.cC]//g"`
+# -m ("max line matches") ... only works for grep 2.5.x
+# -i ("insensitive")
+#
+TEMP=`sensors | grep -i "CPU temp" | cut -d" " -f4 | sed "s/\..*//g" | sed "s/[\+\.cC]//g"`
 
 # this is the real deal
 if [ $TEMP -gt $LIMIT ]; then

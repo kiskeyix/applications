d3a0c86f57d4c1d330dcbac2122e7f3430712310
Luis Mondesi <lemsx1@gmail.com>
2003-02-22 05:23:09 +0000

    optional second argument for revision numbers

--- scripts/make-kpkg.sh
+++ scripts/make-kpkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2003-Feb-09
+# Last modified: 2003-Feb-20
 #
 # DESCRIPTION: Use the wonderful "make-kpkg" from Debian
 #               to build a custom kernel.
@@ -9,9 +9,12 @@
 # USAGE:    cd to /usr/src/linux (or the linux source tree)
 #           and then call:
 #           
-#           make-kpkg.sh ##
+#           make-kpkg.sh #1 #2
 #           
-#               where ## is 01 (or 02, or 03..., etc...)
+#               where #1 is the number appended to the kernel
+#               (01 or 02, or 03..., etc...)
+#               and #2 is the revision of this kernel: 1.0, 1.1 ...
+#               
 # CHANGELOG:
 #
 
@@ -24,9 +27,17 @@ IMAGE_TOP="../" # where to save the resulting .deb files
 
 export IMAGE_TOP ALL_PATCH_DIR PATCH_THE_KERNEL MODULE_LOC
 if [ $1 ]; then
+    if [ $2 ]; then
+        REVISION="$2"
+    else
+        REVISION="1.0"
+    fi
     echo -e "Building kernel"
     make-kpkg clean
-    make-kpkg --rootcmd $FAKEROOT  --initrd --config oldconfig --append-to-version -custom.$1 --revision $1 clean modules_clean kernel_image modules_image
+    make-kpkg --rootcmd $FAKEROOT  --initrd \
+        --config oldconfig --append-to-version -custom.$1 \
+        --revision $REVISION clean modules_clean \
+            kernel_image modules_image
 else
     echo -e "Usage: $0 ## \n \t Where ## is an interger"
 fi

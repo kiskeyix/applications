a349cfdb884e4cf815e582aa675212d619d3a536
Luis Mondesi <lemsx1@gmail.com>
2003-03-10 00:48:45 +0000

    cover most common mistakes

--- scripts/new_site.pl
+++ scripts/new_site.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl -w
-# $Revision: 1.1 $
+# $Revision: 1.2 $
+# $Date: 2003-03-10 00:48:45 $
+#
 # Luis Mondesi < lemsx1@hotmail.com >
 # Last modified: 2003-Mar-09
 #
@@ -9,19 +11,47 @@
 # CHANGELOG:
 
 use Term::ReadLine;
+use Getopt::Long;
+Getopt::Long::Configure('bundling');
 use strict;
-#use Config;
 $|++;
 
 my $term = new Term::ReadLine 'new_site';
 
-my $WELCOME_MSG="";
+my $USE_CPU = 0; # cpu is the LDAP useradd/usermod program
+my $USE_USERADD = 0; # create new user using useradd
+my $HELP=0;
+
+my $USAGE = "new_site.pl [-h|--help] [-u|--useradd] [-c|--cpu]\n";
+
+# get options
+GetOptions(
+    'u|useradd'=>\$USE_USERADD,
+    'c|cpu' => \$USE_CPU,
+    'h|help' => \$HELP,
+);
+
+if ( $HELP ) { 
+    print STDERR $USAGE;
+    exit(1);
+}
+
+if ( $USE_CPU ) {
+    print STDERR "Using 'cpu' to create user\n";
+}
+
+if ( $USE_USERADD ) {
+    print STDERR "Using 'useradd' to create user\n";
+}
+
+# start script
+my $WELCOME_MSG="Welcome to the new_site creator. Please follow prompts:\n";
 
 print $WELCOME_MSG,"\n";
 
-my $SERVER_IP=prompt("Enter server IP:\n");
-my $SITE=prompt("Enter site:\n");
-my $WEBMASTER=prompt("Enter webmaster username:\n");
+my $SERVER_IP=prompt("Enter server IP: ");
+my $SITE=prompt("Enter site: ");
+my $WEBMASTER=prompt("Enter webmaster username: ");
 my $WEBMASTER_EMAIL="$WEBMASTER\@$SITE";
 
 # virtual hosts go to:
@@ -29,30 +59,69 @@ my $APACHE_CONF="/etc/apache/httpd.conf";
 # virtual email go to:
 my $SMTP_VIRTUAL="/etc/postfix/virtual";
 
-$APACHE_CONF=prompt("Enter apache config file[$APACHE_CONF]:\n");
-$SMTP_VIRTUAL=prompt("Enter SMTP virtual config file[$SMTP_VIRTUAL]:\n");
+$APACHE_CONF=prompt("Enter apache config file[$APACHE_CONF]: ",$APACHE_CONF);
+$SMTP_VIRTUAL=prompt("Enter SMTP virtual config file[$SMTP_VIRTUAL]: ",$SMTP_VIRTUAL);
 
 my $APACHE_HOST_TEMPLATE="\n<VirtualHost $SERVER_IP>\nServerAdmin $WEBMASTER_EMAIL\nDocumentRoot /home/$WEBMASTER/$SITE/html\nServerName $SITE\nErrorLog /var/log/apache/$SITE-error.log\nCustomLog /var/log/apache/$SITE-access_log combined\n<Directory />\nAllowOverride FileInfo AuthConfig Limit Options\n</Directory>\n</VirtualHost>\n";
 
-print $SERVER_IP,"\n";
-print $SITE,"\n";
-print $WEBMASTER,"\n";
-print $WEBMASTER_EMAIL,"\n";
+# print output to files:
+# summary to STDOUT
+print STDOUT "Summary:\n";
+print STDOUT "\tServer IP: $SERVER_IP\n";
+print STDOUT "\tSite: $SITE\n";
+print STDOUT "\tWebmaster: $WEBMASTER\n";
+print STDOUT "\tE-Mail: $WEBMASTER_EMAIL\n";
 
-print $APACHE_CONF,"\n";
-print $SMTP_VIRTUAL,"\n";
-print $APACHE_HOST_TEMPLATE,"\n";
+# user was created using the following:
+#
+# useradd -m -d /home/$WEBMASTER -s /bin/false -g popusers $WEBMASTER
+# passwd $WEBMASTER
+# mkdir -p /home/$WEBMASTER/$SITE/html
+# 
+# if LDAP user, just put "cpu" in front of "useradd" above and change
+# -g popusers to -g GID (500). Also, note that cpu asks for a password
+
+if ( $USE_CPU ) {
+    system("useradd -m -d /home/$WEBMASTER -s /bin/false -g 500 $WEBMASTER ");
+} elsif ( $USE_USERADD ) {
+    system("useradd -m -d /home/$WEBMASTER -s /bin/false -g popusers $WEBMASTER ");
+    system("passwd $WEBMASTER");
+}
+
+# append to apache conf file
+open (APACHE,">>$APACHE_CONF") or warn "File $APACHE_CONF could not be open for writing\n";
+print APACHE $APACHE_HOST_TEMPLATE,"\n";
+close(APACHE);
+
+# SMTP virtual file
+open (SMTP,">>$SMTP_VIRTUAL") or warn "File $SMTP_VIRTUAL could not be open for writing\n";
+print SMTP "\@$SITE $WEBMASTER\n";
+
+my $MAKE_MAP="makemap";
+
+if ( $SMTP_VIRTUAL =~ m/postfix/i) {
+    $MAKE_MAP = "postmap";    
+} elsif ( $SMTP_VIRTUAL =~ m/(sendmail|mail)/i ) {
+    $MAKE_MAP = "makemap hash"; 
+}
+
+print STDOUT "Run '$MAKE_MAP' or favorite hash map creator on '$SMTP_VIRTUAL' to activate alias name\n";
+
+# functions
 
 sub prompt {
     # promt user and return input 
     # pass string when calling subroutine: $var = prompt("string");
     
-    my($string) = $_[0];#shift;
+    my($string) = $_[0];
     my($default) = $_[1];
     
     my($input) = "";
     
     $input=$term->readline("* $string");
+    if ($input eq "") {
+        $input = $default;
+    }
     # if not readline support, uncomment these:
     # print ("* ".$string."\n");
     # chomp($input = <STDIN>);

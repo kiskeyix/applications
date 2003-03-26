bebbe3e0a36d64aa0b2656d96aaea631b4df0266
Luis Mondesi <lemsx1@gmail.com>
2003-03-26 01:54:12 +0000

    fixed misleading comment

--- scripts/vnctunnel.sh
+++ scripts/vnctunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $Id: vnctunnel.sh,v 1.1 2003-01-31 09:25:42 luigi Exp $
+# $Id: vnctunnel.sh,v 1.2 2003-03-26 01:54:12 luigi Exp $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2003-Jan-31
+# Last modified: 2003-Mar-25
 #
 # DESCRIPTION:
 # USAGE: $0 SERVER [USER]
@@ -10,7 +10,7 @@
 
 VNCVIEWER=vncviewer
 
-echo "Connect using the command '$VNCVIEWER $1:5900' from a different terminal (at localhost)"
+echo "Connect using the command '$VNCVIEWER localhost:5900'"
 
 if [ $2 ]; then
     ssh -L 5900:localhost:5900 $2@$1

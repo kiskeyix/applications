b2fc230b7285cdfeb2795c94cebd8f3a35b468f4
Luis Mondesi <lemsx1@gmail.com>
2003-06-30 06:27:53 +0000

    New shinny version. Allows configuration files to be passed to it. Works nicely. Also, note the use of 'if defined' when using Perl variables that might not exist to avoid comparing or concatenating variables which are empty. essentially gets rid of the warnings... magically

--- scripts/rss2html.pl
+++ scripts/rss2html.pl
@@ -5,6 +5,28 @@
 # by Jonathan Eisenzopf. v1.0 19990901
 # See http://www.webreference.com/perl for more information
 
+# 2003-06-30 02:16 EDT 
+# modified by Luis Mondesi for the Zaurus. Now it has couple
+# of features:
+# 
+# DESC
+# 
+# 1 - Output is saved to a file at  $HOME/rss2html.html
+# 2 - Links are taken from a file $HOME/.rss2htmlrc. You can
+#     have multiple links separated by new lines ( \n ). You
+#     can also use hashes to comment out lines ( # )
+# You will need to call this from the command line as:
+# rss2html.pl config
+#
+# TODO
+# A .desktop will be included later so that Zaurus users can
+# just click on it... 
+# 
+# NOTE
+# A convenient thing is to just make your $HOME/rss2html.html 
+# file your home page for your browser in the Zaurus :-)
+#
+
 # INCLUDES
 use strict;
 use XML::RSS;
@@ -13,10 +35,21 @@ use LWP::Simple;
 # Declare variables
 my $content;
 my $file;
+# luis 2003-06-29 13:47
+# Let's get our links from this file:
+# this file is composed of a link per line
+# like:
+# http://www.server.com/rdf.rdf
+# http://www.otherserver.com/rss.rss
+# # this is a comment
+# 
+my $config_file = $ENV{'HOME'}."/.rss2htmlrc";
+# information will be outputted to this file
+my $output_file = $ENV{'HOME'}."/rss2html.html";
 
 # MAIN
 # check for command-line argument
-die "Usage: rss2html.pl (<RSS file> | <URL>)\n" unless @ARGV == 1;
+die "Usage: rss2html.pl (<RSS file> | <URL> | config )\n" unless @ARGV == 1;
 
 # get the command-line argument
 my $arg = shift;
@@ -30,22 +63,70 @@ if ($arg=~ /http:/i) {
     die "Could not retrieve $arg" unless $content;
     # parse the RSS content
     $rss->parse($content);
+    
+    # clear up output file
+    open(HTMLF, "> $output_file");
+    close(HTMLF);
+    
+    # print the HTML channel
+    &print_html($rss);
 
+# parse a configuration file
+} elsif ($arg eq "config") {
+    # config file is just a list of URLs, therefore 
+    if (open(CONFIG, "< $config_file")){
+        # possibly a bug, but, this is the best places to
+        # clean up this file:
+        # clear up output file
+        open(HTMLF, "> $output_file");
+        close(HTMLF);
+
+        while (<CONFIG>) {
+            # comments are allowed in the configuration file
+            next if /^\s*#/;
+            chomp;
+            if ($_ =~ /http:/i) {
+                $content = get($_);
+                die "Could not retrieve $_" unless $content;
+                # parse the RSS content
+                $rss->parse($content);
+
+                # print (append) the HTML channel
+                &print_html($rss);
+
+            } #end if http
+        } #end while
+        
+        close(CONFIG);
+
+    } #end if
 # argument is a file
 } else {
     $file = $arg;
     die "File \"$file\" does't exist.\n" unless -e $file;
     # parse the RSS file
     $rss->parsefile($file);
-}
+    # clear up output file
+    open(HTMLF, "> $output_file");
+    close(HTMLF);
 
-# print the HTML channel
-&print_html($rss);
+    # print the HTML channel
+    &print_html($rss);
+
+}
 
 # SUBROUTINES
 sub print_html {
     my $rss = shift;
-    print <<HTML;
+
+    if (open(HTMLF, ">> $output_file")) {
+
+        print HTMLF <<HTML;
+<HTML>
+<head>
+    <title>RSS Feeds Parser:</title>
+</head>
+<body bgcolor="white">
 <table bgcolor="#000000" border="0" width="200"><tr><td>
 <TABLE CELLSPACING="1" CELLPADDING="4" BGCOLOR="#FFFFFF" BORDER=0 width="100%">
   <tr>
@@ -53,53 +134,55 @@ sub print_html {
 <tr><td>
 HTML
 
-    # print channel image
-    if ($rss->{'image'}->{'link'}) {
-	print <<HTML;
+        # print channel image
+        if ($rss->{'image'}->{'link'}) {
+            print HTMLF <<HTML;
 <center>
-<p><a href="$rss->{'image'}->{'link'}"><img src="$rss->{'image'}->{'url'}" alt="$rss->{'image'}->{'title'}" border="0"
+<p><a href="$rss->{'image'}->{'link'}">
+<img src="$rss->{'image'}->{'url'}" alt="$rss->{'image'}->{'title'}" border="0"
 HTML
-        print " width=\"$rss->{'image'}->{'width'}\""
-	    if $rss->{'image'}->{'width'};
-	print " height=\"$rss->{'image'}->{'height'}\""
-	    if $rss->{'image'}->{'height'};
-	print "></a></center><p>\n";
-    }
+            print HTMLF " width=\"$rss->{'image'}->{'width'}\""
+            if $rss->{'image'}->{'width'};
+            print HTMLF " height=\"$rss->{'image'}->{'height'}\""
+            if $rss->{'image'}->{'height'};
+            print HTMLF "></a></center><p>\n";
+        } #end if rss
 
-    # print the channel items
-    foreach my $item (@{$rss->{'items'}}) {
-	next unless defined($item->{'title'}) && defined($item->{'link'});
-	print "<li><a href=\"$item->{'link'}\">$item->{'title'}</a><BR>\n";
-    }
+        # print the channel items
+        foreach my $item (@{$rss->{'items'}}) {
+            next unless defined($item->{'title'}) && defined($item->{'link'});
+            print HTMLF "<li><a href=\"$item->{'link'}\">$item->{'title'}</a><BR>\n";
+            # luis 2003-06-29 13:37 
+            # in addition, I care about the actual text
+            print HTMLF "$item->{'description'}</li><BR>\n" if defined ($item->{'description'});
+        } # end foreach
 
-    # if there's a textinput element
-    if ($rss->{'textinput'}->{'title'}) {
-	print <<HTML;
+        # if there's a textinput element
+        if ($rss->{'textinput'}->{'title'}) {
+            print HTMLF <<HTML;
 <form method="get" action="$rss->{'textinput'}->{'link'}">
 $rss->{'textinput'}->{'description'}<BR> 
 <input type="text" name="$rss->{'textinput'}->{'name'}"><BR>
 <input type="submit" value="$rss->{'textinput'}->{'title'}">
 </form>
 HTML
-    }
+        } #end if rss
 
-    # if there's a copyright element
-    if ($rss->{'channel'}->{'copyright'}) {
-	print <<HTML;
+        # if there's a copyright element
+        if ($rss->{'channel'}->{'copyright'}) {
+            print HTMLF <<HTML;
 <p><sub>$rss->{'channel'}->{'copyright'}</sub></p>
 HTML
-    }
+        } # end if rss
 
-    print <<HTML;
+    print HTMLF <<HTML;
 </td>
 </TR>
 </TABLE>
 </td></tr></table>
+</body></html>
 HTML
-}
-
-
-
-
+    } #end if open
+} #end function
 
 

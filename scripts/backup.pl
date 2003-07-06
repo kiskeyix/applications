0e2cd4788ce92ea35abda36d27e2006490d6d164
Luis Mondesi <lemsx1@gmail.com>
2003-07-06 06:25:36 +0000

    lots of code cleanups and bug fixes. plus introduccion of NAME, TAR, and COMPRESS_DO variables to customize whether we should use Archive::Tar or the user specify archive command

--- scripts/backup.pl
+++ scripts/backup.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# $Revision: 1.14 $
+# $Revision: 1.15 $
 # Luis Mondesi < lemsx1@hotmail.com >
 # Last modified: 2003-Jul-06
 #
@@ -21,6 +21,12 @@
 # Example $HOME/.backuprc
 # # NOTE that the defaults for these variables are sufficient
 # # for a Debian system.
+# TAR=/usr/local/bin/tar
+# COMPRESS_LEVEL=9
+# # bzip2 or gzip? or don't define if no compression is needed
+# COMPRESS_DO=/usr/bin/gzip
+# # prefix to name of the file
+# NAME=imac-home 
 #
 # BAK=/dir/to/store/backups
 # EXCLUDES=.*this.*|.*that\$
@@ -51,6 +57,14 @@
 # > install Archive::Tar
 # 
 # Then follow the prompts.
+# 
+# On any UNIX system you can always opt to defined your own version
+# of tar like:
+# TAR = /usr/bin/tar
+# and whether you want to use gzip or bzip2
+# COMPRESS_DO = /usr/bin/gzip
+# and your compression level low "0" & "9" highest
+# COMPRESS_LEVEL=9
 #
 
 use strict;
@@ -63,24 +77,27 @@ use File::Basename; # basename();
 my $DEBUG = 0;      # set to 1 to print debugging messages
 
 my %MY_CONFIG = ();
-my $CONFIG_FILE= $ENV{HOME}."/.backuprc";
+my $CONFIG_FILE= $ENV{"HOME"}."/.backuprc";
 
-$MY_CONFIG{BAK}="/home/backup";     # default backup directory
+$MY_CONFIG{"NAME"} = "backup";      # default name
+
+$MY_CONFIG{"BAK"}="/home/backup";     # default backup directory
                                     # you might want to change this
                                     # in your .backuprc file like:
                                     # BAK="/other/dir"
 # tar EXCL list regexp. specify EXCLUDES in your .backuprc to modify 
-$MY_CONFIG{EXCLUDES}=".*\.pid\$|.*\.soc\$|.*\.log\$";
+$MY_CONFIG{"EXCLUDES"}=".*\.pid\$|.*\.soc\$|.*\.log\$";
 
+$MY_CONFIG{"COMPRESS_LEVEL"} = "9"; # default compression level
 
-$MY_CONFIG{LOW_UID} = "1000";          # debian standard lowest uid.
+$MY_CONFIG{"LOW_UID"} = "1000";     # debian standard lowest uid.
                                     # change in .backuprc
-$MY_CONFIG{EXC_ULIST} = "man|nobody";  # separated by | . Change in
+$MY_CONFIG{"EXC_ULIST"} = "man|nobody";  # separated by | . Change in
                                     # .backuprc
 
 # backup "root" home dir, cvsroot and other important stuff
 
-$MY_CONFIG{SYSTEM}="/etc /var/mail /var/spool /var/lib/iptables /root";
+$MY_CONFIG{"SYSTEM"}="/etc /var/mail /var/spool /var/lib/iptables /root";
 
 #-------------------------------------------------------------#
 #           No need to modify anything below here             #
@@ -88,12 +105,19 @@ $MY_CONFIG{SYSTEM}="/etc /var/mail /var/spool /var/lib/iptables /root";
 
 my $TMP_LOCK = ".backup-lock";
 
+# init defaults from $CONFIG_FILE
 my %TMP_CONFIG = init_config($CONFIG_FILE); # override defaults with...
 
 my %CONFIG = ();    # where the two will be merged
 my @tmp_files = (); # temporary list of files
 my @filelist = ();  # temp list
 my @tmp_dirs = ();  # temp dirs
+
+my $COMMAND = "";   # init scalar for command
+my $TMP_COMMAND ="";# individual command line options
+my $SYSTEM_COMMAND=""# what's send to the system from past 2 commands
+
+my $USE_TAR = 0;    # use a binary of tar instead of Perl module?
 
 # merge two hashes and warn about dups... use the last defined key=>val
 my ($k, $v) = "";
@@ -104,7 +128,7 @@ foreach my $hashref ( \%MY_CONFIG, \%TMP_CONFIG ) {
             print STDERR "Warning: $k seen twice.  Using the second definition.\n";
             # next;
         }
-        $CONFIG{$k} = $v;
+        $CONFIG{"$k"} = $v;
     }
 }
 
@@ -118,10 +142,10 @@ if ( $DEBUG != 0 ) {
 }
 
 # change to backup directory
-if ( -d $CONFIG{BAK} ) {
-    chdir($CONFIG{BAK});
+if ( -d $CONFIG{"BAK"} ) {
+    chdir($CONFIG{"BAK"});
 } else {
-    die "could not change working dir to $CONFIG{BAK}. $!";
+    die "could not change working dir to ".$CONFIG{"BAK"}." ".$!;
 }
 
 if ( ! -f $TMP_LOCK ) {
@@ -136,14 +160,39 @@ if ( ! -f $TMP_LOCK ) {
     open(FILE,"> $TMP_LOCK") || die "could not open $TMP_LOCK. $! \n";
     print FILE $year."-".$mon."-".$mday." ".$hour.":".$min.":".$sec;
     close(FILE); 
-    
-    if ( exists $CONFIG{SYSTEM} ) {
+  
+    if ( exists $CONFIG{"TAR"} && -x $CONFIG{"TAR"} ) {
+        $USE_TAR = 1;
+        $COMMAND = sprintf("%s cf - xxFILESxx' ",$CONFIG{"TAR"});
+    } elsif ( exists $CONFIG{"TAR"} && $DEBUG !=0 ) {
+        print STDERR "Tar was given but not found! \n";
+    }
+
+
+    if ( exists $CONFIG{"COMPRESS_DO"} && 
+        -x $CONFIG{"COMPRESS_DO"} ) {
+        # file name is STDOUT
+        ( $COMMAND = $COMMAND) =~ s/\%TARFILE\%/-/;
+        # reuse COMMAND from above and 
+        # pipe it to compress utility
+        $COMMAND = sprintf(
+            "%s | %s -%d -c ",
+            $COMMAND,
+            $CONFIG{"COMPRESS_DO"},
+            $CONFIG{"COMPRESS_LEVEL"});
+    } elsif ( exists $CONFIG{"COMPRESS_DO"} && $DEBUG !=0 ) {
+        print STDERR "Compress utility not found! \n";
+    }
+
+    # ========== START BACKUP PROCESS ================= #
+    # System backup
+    if ( exists $CONFIG{"SYSTEM"} ) {
         # backup system
         # Archive::Tar->create_archive ("my.tar.gz", 9, "/this/file", "/that/file");
         @filelist = ();
         # a bit of sanity checking... 
         # check for two spaces or commas in list
-        @tmp_dirs = split(/ +|,+/,$CONFIG{SYSTEM});
+        @tmp_dirs = split(/ +|,+/,$CONFIG{"SYSTEM"});
 
         foreach ( @tmp_dirs ) {
             if ( -d $_ ) {
@@ -154,18 +203,39 @@ if ( ! -f $TMP_LOCK ) {
 
         print STDOUT "Backing up system files \n";
         if ( $DEBUG == 0 ) {
-            Archive::Tar->create_archive (
-                "system-$MIDDLE_STR.tar.gz", 
-                9, 
-                @filelist
-            );
+            if ( $USE_TAR ) {
+                # TODO maybe this should be in a subroutine?
+                # compression is not needed? then use filename
+                my $TMP_FILE_NAME = "$CONFIG{"NAME"}-system-$MIDDLE_STR.tar";
+                # to allow other formats, let's probe one at a time
+                $TMP_FILE_NAME .= ( $CONFIG{"COMPRESS_DO"} =~ m/bzip2/ ) ? ".bz2" : "";
+                $TMP_FILE_NAME .= ( $CONFIG{"COMPRESS_DO"} =~ m/gzip/ ) ? ".gz" : "";
+                
+                my $TMP_FILE_LIST = join(' ',@file_list);
+                # put files and tar file name in place holders
+                ( $TMP_COMMAND = $COMMAND) =~ s/xxFILESxx/$TMP_FILE_LIST/;
+                
+                $SYSTEM_COMMAND = sprintf("%s > %s",
+                    $COMMAND,
+                    $TMP_FILE_NAME);
+                system($SYSTEM_COMMAND);
+                if ( $? !=0 ) {
+                    print STDERR "Command '$SYSTEM_COMMAND' failed terribly!";
+                }
+            } else {
+                Archive::Tar->create_archive (
+                    "$CONFIG{"NAME"}-system-$MIDDLE_STR.tar.gz", 
+                    $CONFIG{"COMPRESS_LEVEL"}, 
+                    @filelist
+                );
+            }
         } # end if debug
-    } # end if $CONFIG{SYSTEM} 
+    } # end if $CONFIG{"SYSTEM"} 
 
     # backup users
     my %user = ();  # user/userdir pair in a hash
 
-    my $users_excluded_pattern = $CONFIG{EXC_ULIST};
+    my $users_excluded_pattern = $CONFIG{"EXC_ULIST"};
 
     while (my @r = getpwent()) {
         # $name,$passwd,$uid,$gid,$quota,$comment,$gcos,$dir,$shell,$expire
@@ -173,7 +243,7 @@ if ( ! -f $TMP_LOCK ) {
         if ( 
             
             $r[0] !~ m/$users_excluded_pattern/i && 
-            $CONFIG{LOW_UID} <= $r[2] 
+            $CONFIG{"LOW_UID"} <= $r[2] 
         ) {
             $user{$r[0]}=$r[7];
             #print $r[0]."\n";
@@ -194,23 +264,49 @@ if ( ! -f $TMP_LOCK ) {
             push(@filelist,@ary);
         }
     }
-    print STDOUT join(" ",@filelist)."\n";
-    
-    print STDOUT "Backing up users files \n";
+    #print STDOUT join(" ",@filelist)."\n";
+   
+    # Users backup
+    print STDOUT "Backing up users files... \n";
     if ( $DEBUG == 0 ) {
-        #Archive::Tar->create_archive (
-        #    "users-$MIDDLE_STR.tar.gz", 
-        #    9, 
-        #    @filelist
-        #);
+        if ( $USE_TAR ) {
+                # TODO maybe this should be in a subroutine?
+                # compression is not needed? then use filename
+                my $TMP_FILE_NAME = "$CONFIG{"NAME"}-user-$MIDDLE_STR.tar";
+                # TODO put these in COMPRESS_DO general above
+                # and declare a $EXT scalar holding the string to use
+                # for all file_names
+                # to allow other formats, let's probe one at a time
+                $TMP_FILE_NAME .= ( $CONFIG{"COMPRESS_DO"} =~ m/bzip2/ ) ? ".bz2" : "";
+                $TMP_FILE_NAME .= ( $CONFIG{"COMPRESS_DO"} =~ m/gzip/ ) ? ".gz" : "";
+                
+                my $TMP_FILE_LIST = join(' ',@file_list);
+                # put files and tar file name in place holders
+                ( $TMP_COMMAND = $COMMAND) =~ s/xxFILESxx/$TMP_FILE_LIST/;
+                
+                $SYSTEM_COMMAND = sprintf("%s > %s",
+                    $TMP_COMMAND,
+                    $TMP_FILE_NAME);
+                system($SYSTEM_COMMAND);
+                if ( $? !=0 ) {
+                    print STDERR "Command '$SYSTEM_COMMAND' failed terribly!";
+                }
+        } else {
+            Archive::Tar->create_archive (
+                "$CONFIG{"NAME"}-users-$MIDDLE_STR.tar.gz", 
+                $CONFIG{"COMPRESS_LEVEL"}, 
+                @filelist
+            );
+        }
     } # end if debug
 
-    if ( exists $CONFIG{DIRS} ) {
+    # Other directories ( non-system specific )
+    if ( exists $CONFIG{"DIRS"} ) {
         # backup others
         @filelist = ();
         # a bit of sanity checking... 
         # check for two spaces or commas in list
-        @tmp_dirs = split(/ +|,+/,$CONFIG{DIRS});
+        @tmp_dirs = split(/ +|,+/,$CONFIG{"DIRS"});
 
         foreach ( @tmp_dirs ) {
             if ( -d $_ ) {
@@ -219,16 +315,42 @@ if ( ! -f $TMP_LOCK ) {
             }
         }
 
-        print STDOUT "Backing up other files $CONFIG{DIRS} \n";
+        print STDOUT "Backing up other files $CONFIG{"DIRS"} \n";
 
         if ( $DEBUG == 0 ) {
-            Archive::Tar->create_archive (
-                "other-$MIDDLE_STR.tar.gz", 
-                9, 
-                @filelist
-            );
+            if ( $USE_TAR ) {
+                # TODO maybe this should be in a subroutine?
+                # compression is not needed? then use filename
+                my $TMP_FILE_NAME = "$CONFIG{"NAME"}-other-$MIDDLE_STR.tar";
+                # TODO put these in COMPRESS_DO general above
+                # and declare a $EXT scalar holding the string to use
+                # for all file_names
+                # to allow other formats, let's probe one at a time
+                $TMP_FILE_NAME .= ( $CONFIG{"COMPRESS_DO"} =~ m/bzip2/ ) ? ".bz2" : "";
+                $TMP_FILE_NAME .= ( $CONFIG{"COMPRESS_DO"} =~ m/gzip/ ) ? ".gz" : "";
+
+                my $TMP_FILE_LIST = join(' ',@file_list);
+                # put files and tar file name in place holders
+                ( $TMP_COMMAND = $COMMAND) =~ s/xxFILESxx/$TMP_FILE_LIST/;
+
+                $SYSTEM_COMMAND = sprintf("%s > %s",
+                    $TMP_COMMAND,
+                    $TMP_FILE_NAME);
+                system($SYSTEM_COMMAND);
+                if ( $? !=0 ) {
+                    print STDERR "Command '$SYSTEM_COMMAND' failed terribly!";
+                }
+            } else {
+                Archive::Tar->create_archive (
+                    "$CONFIG{"NAME"}-other-$MIDDLE_STR.tar.gz", 
+                    $CONFIG{"COMPRESS_LEVEL"}, 
+                    @filelist
+                );
+            }
         } # end if debug
-    } # end if $CONFIG{DIRS}
+    } # end if $CONFIG{"DIRS"}
+
+    # ++++++++++ END BACKUP PROCESS ++++++++++ #
 
     # debian specific
     if ( -f "/etc/debian_version" ) {
@@ -239,13 +361,15 @@ if ( ! -f $TMP_LOCK ) {
             print STDOUT "Debian selections file created as: selections.txt.\n Use:\n dpkg --set-selections < selections.txt && dselect update \n to restore from this list.";
         }
     }
+
+    # gracely exit...
+    unlink "$TMP_LOCK" or die "could not remove ".$TMP_LOCK.". $!\n";
+
 } else {
-    die "Lock file ".$CONFIG{BAK}."/$TMP_LOCK exists... exiting.\n";
+    die "Lock file ".$CONFIG{"BAK"}."/$TMP_LOCK exists... exiting.\n";
 }
 
-# gracely exit...
-unlink "$TMP_LOCK" or die "could not remove ".$TMP_LOCK.". $!\n";
-
+# ------------------------------------------------------#
 # functions 
 
 sub init_config {
@@ -258,7 +382,7 @@ sub init_config {
     # 
     # to
     # 
-    # hash{VAR}='argument'
+    # hash{"VAR"}='argument'
     
     my %config_tmp = ();
 
@@ -312,7 +436,7 @@ sub process_file {
     my $base_name = basename($_);
     if ( 
         -f $_ && 
-        $base_name !~ m/$CONFIG{EXCLUDES}/ 
+        $base_name !~ m/$CONFIG{"EXCLUDES"}/ 
     ) {
         push @tmp_files,clean("$_") ;
     }

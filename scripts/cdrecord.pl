3a58fc13409c549e4f79d2c169b658a567173d9c
Luis Mondesi <lemsx1@gmail.com>
2003-07-24 05:18:03 +0000

    a nautilus script to burn discs isos

---
+++ scripts/cdrecord.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl -w
+# a quick nautilus script to burn isos... 
+# Just select the iso you want and choose this script
+# from the nautilus script menu.
+
+`cdrecord $ENV{"NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"}`;

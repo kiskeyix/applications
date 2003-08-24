8559087b59f7523358b3bb421778d938f4f9e9b7
Luis Mondesi <lemsx1@gmail.com>
2003-08-24 02:53:07 +0000

    added port to list of arguments

--- scripts/vnctunnel.sh
+++ scripts/vnctunnel.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
-# $Id: vnctunnel.sh,v 1.2 2003-03-26 01:54:12 luigi Exp $
+# $Id: vnctunnel.sh,v 1.3 2003-08-24 02:53:07 luigi Exp $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2003-Mar-25
+# Last modified: 2003-Aug-18
 #
 # DESCRIPTION:
-# USAGE: $0 SERVER [USER]
-# CHANGELOG:
-#
+
+echo "USAGE: $0 SERVER [USER] [PORT]"
 
 VNCVIEWER=vncviewer
+# ssh arguments
+ARG=" -X "
 
-echo "Connect using the command '$VNCVIEWER localhost:5900'"
-
-if [ $2 ]; then
-    ssh -L 5900:localhost:5900 $2@$1
+if [ $3 -ge 5900 ]; then
+    echo "Connect using the command '$VNCVIEWER localhost:$3'"
 else
-    ssh -L 5900:localhost:5900 $1
+    echo "Connect using the command '$VNCVIEWER localhost:5900'"
+fi
+
+if [ $2 -a ! $3 ]; then
+    ssh $ARG -L 5900:localhost:5900 $2@$1
+elif [ $3 -ge 5900 ]; then
+    ssh $ARG -L $3:localhost:$3 $2@$1
+else
+    ssh $ARG -L 5900:localhost:5900 $1
 fi
 
 

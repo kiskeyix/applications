e2361a8dad683dd85f34337cf5435728f42b65a0
Luis Mondesi <lemsx1@gmail.com>
2003-09-10 05:21:53 +0000

    fixed some bugs

--- scripts/vnctunnel.sh
+++ scripts/vnctunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $Id: vnctunnel.sh,v 1.4 2003-08-30 00:34:13 luigi Exp $
+# $Id: vnctunnel.sh,v 1.5 2003-09-10 05:21:53 luigi Exp $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2003-Aug-29
+# Last modified: 2003-Sep-10
 #
 # DESCRIPTION:
 
@@ -11,15 +11,16 @@ VNCVIEWER=vncviewer
 # ssh arguments
 ARG=" -X "
 
-if [ $3 -a $3 -ge 5900 ]; then
+if [ x$3 != "x" ]; then
+    #  -a $3 -ge 5900
     echo "Connect using the command '$VNCVIEWER localhost:$3'"
 else
     echo "Connect using the command '$VNCVIEWER localhost:5900'"
 fi
 
-if [ $2 -a ! $3 ]; then
+if [ x$2 != "x" -a  x$3 == "x" ]; then
     ssh $ARG -L 5900:localhost:5900 $2@$1
-elif [ $3 -ge 5900 ]; then
+elif [ x$3 != "x" ]; then
     ssh $ARG -L $3:localhost:$3 $2@$1
 else
     ssh $ARG -L 5900:localhost:5900 $1

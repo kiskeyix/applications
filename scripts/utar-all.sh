92fb6c54d05b4581177357f36aca52b6c6b10894
Luis Mondesi <lemsx1@gmail.com>
2003-09-26 20:22:55 +0000

    minor modifications

--- scripts/utar-all.sh
+++ scripts/utar-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Last modified: 2003-Feb-05
+# Last modified: 2003-Sep-26
 # Luis Mondesi < lemsx1@hotmail.com >
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 # 
 # DESCRIPTION: use this script to untar
 # a whole directory with extensions: .bz2
@@ -9,13 +9,19 @@
 
 if [ -d $1 ]; then
     echo -e "looking for .bz2 \n";
-    for i in `ls $1/*.bz2`; do
+    for i in `ls $1/*tar.bz2`; do
         #echo -e "$i \n";
         tar -xjvf $i;
     done
+    for i in `ls $1/*.tbz2`; do
+        tar -xzvf $i;
+    done
 
     echo -e "looking for .*gz \n";
-    for i in `ls $1/*.*gz`; do
+    for i in `ls $1/*tar.gz`; do
+        tar -xzvf $i;
+    done
+    for i in `ls $1/*.tgz`; do
         tar -xzvf $i;
     done
 else

9837e3e5614ede70764d7736de6ba13703a88237
Luis Mondesi <lemsx1@gmail.com>
2003-09-29 18:15:21 +0000

    file deleted from current tree

--- cvsphplib.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cvs -z3 -d:pserver:anonymous@cvs.phplib.sourceforge.net:/cvsroot/phplib co php-lib;
--- cvsphpslash-ft.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cvs -z3 -d:pserver:anonymous@cvs.phpslash.sourceforge.net:/cvsroot/phpslash co phpslash-ft;

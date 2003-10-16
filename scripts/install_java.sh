a3bb7e15c1fc59ba765acde543409c2e5015de23
Luis Mondesi <lemsx1@gmail.com>
2003-10-16 00:22:54 +0000

    minor bugs fixed. added gnome desktop icon

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # Luis Mondesi < lemsx1@hotmail.com >
 # Last modified: 2003-Oct-15
 #
 # DESCRIPTION: a simple debian script to install java from Sun. This is
-#               for java version 1.4.2 and up
+#               for java version 1.4.2 and up. You must this as root
 # USAGE: sudo $0
 # CHANGELOG:
 #
 
-MESSAGE="Please get the java binary from http://www.java.com and pass it to this installation as an argument. i.e. ./install_java j2r3-version-linux-i586.bin"
+MESSAGE="Please get the java binary from http://www.java.com and pass it to this installation as an argument. i.e. ./install_java j2re-version-linux-i586.bin"
+
+TMP_JAVA="$1"
 
 if [ x$1 == "xgetjava" ]; then
     echo "Trying to get the java binaries from Sun"
+    cd /tmp
     wget http://sunsdlc1-13-vhost1.cam-colo.bbnplanet.com/servlet/EComFileServlet/main_products/SDLC//ESD5/JSCDL/j2sdk/1.4.2_01/j2re-1_4_2_01-linux-i586.bin
     if [ $? !=0 ]; then
         echo "Download failed"        
         echo $MESSAGE
-        exit(1);
+        exit 1
     fi
+    TMP_JAVA="/tmp/j2re-1_4_2_01-linux-i586.bin"
 fi
 
-if [ -f $1 ]; then
+if [ -f "$TMP_JAVA" ]; then
     WD=`pwd`
     FILE=`basename $1`
     cd /usr/local
@@ -30,7 +34,7 @@ if [ -f $1 ]; then
         echo "Extracting $1 failed"
         echo $MESSAGE
         cd $WD
-        exit(1)
+        exit 1
     else
         # TODO  
         # the name of the extracted file might not be j2re-1_4_2...
@@ -40,7 +44,7 @@ if [ -f $1 ]; then
             echo "Renaming /usr/local/j2re-1_4_2_01 to /usr/local/j2re failed"
             echo "Please make sure this directory exists so that we can proceed"
             echo "After that, you can just call this script without any arguments"
-            exit(1)
+            exit 1
         fi
     fi
 fi 
@@ -53,17 +57,29 @@ if [ -d "/usr/local/MozillaFirebird/plugins" ]; then
     libjavaplugin_oji.so \
     /usr/local/j2re/plugin/i386/ns610-gcc32/libjavaplugin_oji.so \
     50
+    echo "Putting Mozilla Firebird in Gnome-2 menu"
+    echo "[Desktop Entry]" > /usr/share/applications/mozilla-firebird.desktop
+    echo "Name=Mozilla Firebird" >> /usr/share/applications/mozilla-firebird.desktop
+    echo "Comment=Firebird Web Browser"  >> /usr/share/applications/mozilla-firebird.desktop
+    echo "Exec=mozilla-firebird %U"  >> /usr/share/applications/mozilla-firebird.desktop
+    echo "Terminal=false"  >> /usr/share/applications/mozilla-firebird.desktop
+    echo "MultipleArgs=false"  >> /usr/share/applications/mozilla-firebird.desktop
+    echo "Type=Application"  >> /usr/share/applications/mozilla-firebird.desktop
+    echo "Icon=web-browser"  >> /usr/share/applications/mozilla-firebird.desktop
+    echo "Categories=Application;Network"  >> /usr/share/applications/mozilla-firebird.desktop
+
 elif [ -d "/usr/lib/mozilla-firebird/plugins" ]; then
     update-alternatives \
     --install /usr/local/MozillaFirebird/plugins/libjavaplugin_oji.so \
     libjavaplugin_oji.so \
     /usr/local/j2re/plugin/i386/ns610-gcc32/libjavaplugin_oji.so \
     50
-else
-    update-alternatives \
+fi
+
+# put in standard mozilla location also
+update-alternatives \
     --install /usr/lib/mozilla/plugins/libjavaplugin_oji.so \
     libjavaplugin_oji.so \
     /usr/local/j2re/plugin/i386/ns610-gcc32/libjavaplugin_oji.so \
     50
-fi
 

a0028f104965e1c129bb01128fb0598f32b59127
Luis Mondesi <lemsx1@gmail.com>
2003-10-25 06:24:11 +0000

    use Zenity to display a gtk2 dialog

--- scripts/todays_fortune.sh
+++ scripts/todays_fortune.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2003-Oct-14
+# Last modified: 2003-Oct-25
 #
 # DESCRIPTION: uses fortunes and Xdialog to display a short fortune
 #               to the user
@@ -9,8 +9,19 @@
 # CHANGELOG:
 #
 
+PATH=/usr/local/bin:/usr/bin:/bin:$PATH
+
 GTKRC="/usr/share/themes/Nuvola/gtk-1.0/gtkrc"
-Xdialog --rc-file "$GTKRC" \
+
+if [ -x "/usr/bin/zenity" ]; then
+    zenity --info \
+        --text="`/usr/games/fortune -s`" \
+        --title="Fortune" 
+        
+        #--calendar
+else
+    
+    Xdialog --rc-file "$GTKRC" \
         --backtitle "Today's Fortune" \
         --title "Fortune" \
         --allow-close \
@@ -18,4 +29,4 @@ Xdialog --rc-file "$GTKRC" \
         --left \
         --msgbox "`/usr/games/fortune -s`" \
         0 0
-
+fi

b06489ba09739689b427cea1bb693b898e539e80
Luis Mondesi <lemsx1@gmail.com>
2003-12-01 05:32:26 +0000

    minor fixes

--- scripts/mkisofs.pl
+++ scripts/mkisofs.pl
@@ -3,6 +3,19 @@
 # Just select the directory you want and choose this script
 # from the nautilus script menu.
 
+$DEBUG=1;
+
+# You could get only the selected files from nautilus, but
+# it's better to let the user put all those files in 
+# a single directory and then run this script on that directory
+# $ENV{"NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"}
+
+# Volume id needs no spaces or other characters
+( $volumeid = $ARGV[0] ) =~ s,\s+,_,g;
 # removes end-lines and put a .iso extension
-( $name = $ENV{"NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"} ) =~ s,\n,.iso,g;
-`mkisofs -J -r -v -o "$name" $ENV{"NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"}`;
+( $name = $ARGV[0] ) =~ s,(\w+),$1.iso,g;
+
+print "$ARGV[0] : $volumeid => $name\n" if $DEBUG == 1;
+
+`mkisofs -J -r -v -o "$name" -V $volumeid $ARGV[0]`;
+#eof

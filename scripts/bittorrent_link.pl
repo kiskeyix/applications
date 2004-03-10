75f44aa6793fe64b8f1d6aa9d4233990c1082eae
Luis Mondesi <lemsx1@gmail.com>
2004-03-10 05:39:23 +0000

    fixed minor bugs

--- scripts/bittorrent_link.pl
+++ scripts/bittorrent_link.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2004-Mar-08
+# Last modified: 2004-Mar-10
 #
 # DESCRIPTION: creates a .torrent file in the local directory
 #               which will be used to link with an announcer (tracker)
@@ -16,22 +16,34 @@ use File::Copy qw(move);
 
 my $DEBUG = 0; # set to 1 to debug script
 
+# logging is not necessary, but it helps. You can set this
+# as you see fit...
+my $LOG_DIR = "log";
+if ( !-d $LOG_DIR )
+{
+    mkdir($LOG_DIR) or die("Could not create log dir. $!");
+}
 # point these two to /dev/null if you don't care about logging
-my $CLIENT_LOG = "client_log.$$"; # keeps btdownload* logs
-my $TRACKER_LOG = "tracker_log.$$"; # keeps tracker logs (if running local)
+my $CLIENT_LOG = "$LOG_DIR/client_log.$$"; # keeps btdownload* logs
+my $TRACKER_LOG = "$LOG_DIR/tracker_log.$$"; # keeps tracker logs (if running local)
 
-my $URL ="http://www.latinomixed.com"; # visible URL or public IP for tracker
+# applications
+my $BTMAKEMETAFILE = "btmakemetafile ";
+my $BTDOWNLOAD = "btdownloadheadless ";
+
+# switches for applications
+my $HOST ="www.latinomixed.com";
+my $URL ="http://$HOST"; # visible URL or public IP for tracker
 my $URL_HTTP_PORT = "81"; # standard is 80
 my $BTPORT = "6900"; # usually 6969
 my $PATH_TO_DFILE = "/var/www/a/dstate"; # path to dstate file
-# whole command for bttrack:
+
+my $BTUPLOAD_LIMIT=" --max_upload_rate 7 "; # number of kB/s
+
+# constructing whole command for bttrack:
 my $BTTRACK = "bttrack --port $BTPORT --dfile $PATH_TO_DFILE"; 
-
-my $TRACKER_URL=$URL.":".$BTPORT."/announce"; # url construct for tracker
-
-my $BTMAKEMETAFILE = "btmakemetafile ";
-my $BTDOWNLOAD = "btdownloadheadless ";
-my $BTUPLOAD_LIMIT=" --max_upload_rate 5 "; # number of kB/s
+# constructing whole URL for tracker:
+my $TRACKER_URL=$URL.":".$BTPORT."/announce"; 
 
 # ================================================== #
 #            NO NEED TO MODIFY BELOW THIS LINE       #
@@ -48,6 +60,12 @@ if ( -e $ARGV[0] )
     chomp($rep);
     if ( $rep =~ /^y/i )
     {
+        if ( -f "$PATH_TO_DFILE" ) 
+        {
+            print STDOUT "Removing $PATH_TO_DFILE\n";
+            unlink("$PATH_TO_DFILE") 
+                or warn("Could not remove $PATH_TO_DFILE. You might have to do it manually.$!");
+        }
         print STDERR "Executing:\n$BTTRACK >> $TRACKER_LOG 2>&1 &\n" if $DEBUG;
         system("$BTTRACK >> $TRACKER_LOG 2>&1 &")
             and print STDOUT "check file $TRACKER_LOG for progress\n";
@@ -67,7 +85,10 @@ if ( -e $ARGV[0] )
     }
     if ( -f "$BASENAME" )  
     {
-        my $BT = "$BTDOWNLOAD $BTUPLOAD_LIMIT --url $URL\:$URL_HTTP_PORT/$BASENAME --saveas $ARGV[0]";
+        # assuming that btdownload is the same box as the one running
+        # the tracker, thus the '--ip $HOST' switch. Remove this is
+        # this assumption is incorrent
+        my $BT = "$BTDOWNLOAD $BTUPLOAD_LIMIT --ip $HOST --url $URL\:$URL_HTTP_PORT/$BASENAME --saveas $ARGV[0]";
         print STDERR "Executing:\n$BT >> $CLIENT_LOG 2>&1 &\n" 
             if $DEBUG;
         system("$BT >> $CLIENT_LOG 2>&1 &")

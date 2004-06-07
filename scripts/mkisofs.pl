773c597e00f3ad84e7b2a64006efd4e7bb4e11ad
Luis Mondesi <lemsx1@gmail.com>
2004-06-07 21:52:13 +0000

    new smarter script

--- scripts/mkisofs.pl
+++ scripts/mkisofs.pl
@@ -7,41 +7,57 @@
 # mkisofs.pl DIR
 # or
 # mkisofs.pl DIR dvd # to make a DVD image
+use strict;
+$|++;
 
-$DEBUG=0;
-$VOLIDMAXLENGTH=32;
+my $DEBUG=0;
+my $VOLIDMAXLENGTH=32;
 
 # You could get only the selected files from nautilus, but
 # it's better to let the user put all those files in 
 # a single directory and then run this script on that directory
 # $ENV{"NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"}
 
-chomp($ARGV[0]); # remove end-line
-
+my $folder = $ARGV[0];
+chomp($folder); # remove end-line
+my $volumeid="";
 # Volume id needs no spaces or other characters
-( $volumeid = $ARGV[0] ) =~ s,[\s-]+,_,g;
+( $volumeid = $folder ) =~ s,[\s-]+,_,g;
 # length should be less than $VOLIDMAXLENGTH
 #my $str_length = length ($volumeid);
 $volumeid = substr($volumeid,0,$VOLIDMAXLENGTH);
 # put a .iso extension
-( $name = $ARGV[0] ) =~ s,(.+),$1.iso,;
+my $name = $folder.".iso";
+my $nice =  ( -x "/usr/bin/nice" ) ? "/usr/bin/nice":"";
 
 print "Argument: $ARGV[0] | Volume Name: $volumeid | FileName: $name\n" if $DEBUG == 1;
 sleep(5) if $DEBUG == 1;
 
-my $folder = $ARGV[0];
-
-if ( $ARGV[1] eq "dvd" ) 
+if ( $ARGV[1] && $ARGV[1] eq "dvd" ) 
 {
     die("Directory is not valid DVD tree. Missing $folder/VIDEO_TS") if ( !-d "$folder/VIDEO_TS");
     mkdir("$folder/AUDIO_TS"); # we can afford to try this
     # fix permissions
-    system("chmod 0555 '$folder'");
-    system("find '$folder' -type d -exec chmod 0555 {} \\; ");
-    system("find '$folder' -type f -exec chmod 0444 {} \\; ");
+    m_system("chmod 0555 '$folder'");
+    m_system("$nice find '$folder' -type d -exec chmod 0555 {} \\; ");
+    m_system("$nice find '$folder' -type f -exec chmod 0444 {} \\; ");
     # make iso
-    system("mkisofs -dvd-video -udf -o '$name' -V '$volumeid' '$folder'");
+    m_system("$nice mkisofs -dvd-video -udf -o '$name' -V '$volumeid' '$folder'");
 } else {
-    system("mkisofs -J -r -v -o '$name' -V '$volumeid' '$folder' ");
+    m_system("$nice mkisofs -J -r -v -o '$name' -V '$volumeid' '$folder' ");
 }
+sub m_system
+{
+    my $cmd = shift;
+    if ( $DEBUG )
+    {
+        print STDOUT $cmd."\n";
+    }
+
+    if ( system($cmd) )
+    {
+        print STDERR $!."\n";
+    }
+}
+
 #eof

d1208b69a0e60513b619262ad6d3207df714fd32
Luis Mondesi <lemsx1@gmail.com>
2004-09-09 17:12:24 +0000

    fixed. only needs two arguments now

--- scripts/vnctunnel.sh
+++ scripts/vnctunnel.sh
@@ -1,30 +1,28 @@
 #!/bin/sh
-# $Id: vnctunnel.sh,v 1.7 2004-01-19 02:34:32 luigi Exp $
+# $Id: vnctunnel.sh,v 1.8 2004-09-09 17:12:24 luigi Exp $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2004-Jan-18
+# Last modified: 2004-Sep-08
 #
 # DESCRIPTION: creates a tunnel between 2 servers for Vnc
 
-echo "USAGE: $0 SERVER [USER] [PORT]"
+echo "USAGE: $0 [USER@]SERVER [PORT]"
 
+LOCALHOST="127.0.0.1"
 VNCVIEWER=vncviewer
 # ssh arguments
 ARG=" -X "
 DPORT="5901" # default port
 
-if [ x$3 != "x" ]; then
-    #  -a $3 -ge 5900
-    echo "Connect using the command '$VNCVIEWER 127.0.0.1:$3'"
+if [ x$2 != "x" ]; then
+    echo "Connect using the command: $VNCVIEWER $LOCALHOST:$2"
 else
-    echo "Connect using the command '$VNCVIEWER 127.0.0.1:$DPORT'"
+    echo "Connect using the command: $VNCVIEWER $LOCALHOST:$DPORT"
 fi
 
-if [ x$2 != "x" -a  x$3 = "x" ]; then
-    ssh $ARG -L $DPORT:127.0.0.1:$DPORT $2@$1
-elif [ x$3 != "x" ]; then
-    ssh $ARG -L $3:127.0.0.1:$3 $2@$1
+if [ x$2 != "x" ]; then
+    ssh $ARG -L $2:$LOCALHOST:$2 "$1"
 else
-    ssh $ARG -L $DPORT:127.0.0.1:$DPORT $1
+    ssh $ARG -L $DPORT:$LOCALHOST:$DPORT "$1"
 fi
 
 

19006fc6e7c0204cb29c0229e811c70262afb207
Luis Mondesi <lemsx1@gmail.com>
2004-09-12 19:12:21 +0000

    added support for greping for a given port

--- scripts/mylisten.sh
+++ scripts/mylisten.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-# $Revision: 1.2 $
-# Luis Mondesi < lemsx1@hotmail.com
+# $Revision: 1.3 $
+# Luis Mondesi < lemsx1@hotmail.com >
 #
 # Prints tcp and udp connections that are listening
 # similar to "netstat -l"
-netstat -na | egrep -i "(tcp|udp)"
+if [ $1 ]; then
+    netstat -na | egrep -i "(tcp|udp)" | egrep -i "$1"
+else
+    netstat -na | egrep -i "(tcp|udp)"
+fi

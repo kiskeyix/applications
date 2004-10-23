77b8fd6135636d988f87ac4c5a8604837563a79c
Luis Mondesi <lemsx1@gmail.com>
2004-10-23 05:59:58 +0000

    fixed some bugs

--- scripts/startseti.sh
+++ scripts/startseti.sh
@@ -17,15 +17,15 @@
 #     this can easily be modified, but it's usually the case.
 #
 # USAGE: startseti.sh [start|stop|constop] &
-#   start       - starts seti if LOAD is less than MAXLOAD
+#   start       - starts seti if LOAD is less than MAXLOAD (optional)
 #   stop        - stops seti unconditionally
 #   condstop    - stops seti if LOAD is greater or equal to MAXLOAD
 #
 # TIPS: 
 # Usually is better to start from a cron job like the following:
 # # minute hour dayofmonth month dayofweek user    command
-# 00 */4 * * 1-5 ~/bin/startseti.sh start &
-# */12 * * * *   ~/bin/startseti.sh condstop
+# 00 */4 * * 1-5 exec ~/bin/startseti.sh start &
+# */12 * * * *   exec ~/bin/startseti.sh condstop
 # 
 # This will start seti everyday 4 hours, if not already running,
 # from monday through friday
@@ -71,13 +71,12 @@ PIDOF="/bin/pidof"              # pidof returns process id
 ######################################################################
 
 CPID=$$ # current process id is saved
-EXIT=1  # always assume we will need to exit, 
-        # unless we are doing condstop
 
 HOSTNAME="`hostname`"
+EXIT=1 # assume we want to kill all $PROCESS automatically when we exit
 
-if [ ! $1 ]; then
-    echo "Usage: $0 start|stop|condstop"
+if [ x$1 = "xhelp" -o x$1 = "x--help" -o x$1 = "x-help" -o x$1 = "x-?" ]; then
+    echo "Usage: $0 [start|stop|condstop]"
     exit 1
 fi
 
@@ -85,21 +84,19 @@ fi
 # Functions
 #
 
-# trap some signals:
-# our exit function removes the .pid file
+## trap some signals:
+## our exit function removes the .pid file
 function _exit()        # function to run upon exit of shell
 {
-    # we exit when EXIT is not doing condstop and LOAD is not gt MAXLOAD
-    if [ $EXIT -ne 0 ]; then
-        rm -f $PIDF # removes PID file of this script
+    if [ $EXIT -eq 1 ]; then
         $PKILL $PROCESS # kill all seti processes by me
         if [ $DEBUG -ne 0 ]; then
             echo "Hasta la vista seti $HOSTNAME"
         fi
-        # reenable signals
-        trap EXIT HUP TERM INT
-        exit 0
-    fi
+    fi  
+    # reenable signals
+    trap EXIT HUP TERM INT
+    exit 0
 }
 # hint: trap -l (displays signals)
 # SIGTERM (15) is sent to us from init when rebooting
@@ -108,30 +105,59 @@ trap _exit EXIT HUP TERM INT
 function killold ()
 {
     if [ $DEBUG -ne 0 ]; then
+        echo "" # yeah, echo -e "\nstr\n" but...
         echo "Called killold"
     fi
     SUCCESS=0 # assume we will fail
+    OLD_PID=""
+    if [ -f $PIDF ]; then
+        OLD_PID=`$CAT $PIDF`
+    fi
     MPID=$($PIDOF -x $0)
     for i in $MPID; do
-        if [ x`$CAT $PIDF` = x$i -a x$CPID != x$i ]; then
+        if [ x$OLD_PID = x$i ]; then
             # kill startseti.sh old PID
-            echo "Killing $0 number $i at $HOSTNAME"
-            kill -9 `$CAT $PIDF` &&\
-            rm -f $PIDF &&\
+            echo "Killing $0 with PID number $i at $HOSTNAME"
+            kill -9 $i
+            rm -f $PIDF
             SUCCESS=1
         elif [ x$CPID != x$i ]; then
             echo "Murdering $0 number $i at $HOSTNAME"
-            kill -9 $i &&\
+            kill -9 $i
             SUCCESS=1
         elif [ $DEBUG -ne 0 ]; then
+            # we don't kill ourselves! that would be suicide
             echo "Not killing $i [because we are $CPID]"
         fi
     done
-    if [ $SUCCESS ]; then
-        return 0
-    else
-        return 1
+    return $SUCCESS;
+}
+
+function is_running ()
+{
+    if [ $DEBUG -ne 0 ]; then
+        echo -n "Checking if $0 is running "
     fi
+    IS_RUNNING=0 # assume is not running
+    OLD_PID=""
+    if [ -f $PIDF ];then 
+       OLD_PID=`$CAT $PIDF`
+    fi
+    # get all process id's for all running processes
+    MPID=$($PIDOF -x $0)
+    # determine if the process id is for an older process
+    for i in $MPID; do
+        if [ -f $PIDF -a x$OLD_PID = x$i ]; then
+            if [ $DEBUG -ne 0 ]; then
+                echo "$0 is already running at $HOSTNAME [$i]"
+            fi
+            IS_RUNNING=1
+        fi
+    done
+    if [ $DEBUG -ne 0 ]; then
+        echo "[$IS_RUNNING]"
+    fi
+    return $IS_RUNNING
 }
 
 cd $SETI || exit 1
@@ -144,7 +170,6 @@ LOAD=$(uptime|sed -e "s/.*: \([^,]*\).*/\1/" -e "s/ //g" -e "s/^\([0-9]\+\)\..*/
 # LOAD > MAXLOAD and condstop are set, we stop OR stop was passed to us
 if [ $LOAD -ge $MAXLOAD -a x$1 = "xcondstop" ]; then
     # stop setiathome but leave startseti.sh running
-    EXIT=0 # do not kill previous startseti.sh
     echo -n "Load is high at $HOSTNAME. Stopping $PROCESS "
     $PKILL $PROCESS &&\
         echo "[ok]" &&\
@@ -152,69 +177,58 @@ if [ $LOAD -ge $MAXLOAD -a x$1 = "xcondstop" ]; then
     # else
     echo "[failed]" && exit 1
 elif [ x$1 = "xstop" ]; then
-    # completely stop seti and startseti.sh
+    # completely stop seti and startseti.sh remove all pid files
     echo -n "Stopping $PROCESS at $HOSTNAME " 
-    # cleanup startseti.sh processes if any...
-    $PKILL $PROCESS && \
-        echo "[ok]"
-    killold && exit 0
-    # else echo failed and exit 1
-    echo "[failed]" && exit 1
+    # kill seti_at_home process:
+    $PKILL $PROCESS
+    # cleanup old startseti.sh processes if any...
+    killold 
+    exit 0
 elif [ x$1 = "xcondstop" ]; then
     # load is not high enough, exit
+    EXIT=0 # but do not kill all PROCESSes when exiting...
     if [ $DEBUG -ne 0 ]; then
         echo "Load no high enough at $HOSTNAME [$LOAD < $MAXLOAD]"
     fi
-    EXIT=0 # do not kill previous startseti.sh
     exit 0
 fi
 
-# Assuming a *stop argument won't passed previous checks...
+# Assuming a [cond]stop argument won't passed previous checks...
 # then:
-# are we starting?
+# are we starting? [start arg is optional]
 # if yes, create pid file
 # else
 # try to guess if startseti.sh that is running is old
-if [ x$1 = "xstart" -a ! -f $PIDF ]; then
+START=0
+if [ ! is_running -o x$1 = "xstart" -o x$1 = "x"  -o x$1 = "xlog" ]; then
     echo $CPID > $PIDF 
+    START=1
 else
-    RUNNING=0 # assume startseti.sh ($0) is not running
-    MPID=$($PIDOF -x $0)
-    for i in $MPID; do
-        if [ x`$CAT $PIDF` = x$i ]; then
-            if [ $DEBUG -ne 0 ]; then
-                echo "$0 is already running at $HOSTNAME"
-            fi
-            RUNNING=1
-        fi
-    done
-    if [ $RUNNING -eq 0 -a $LOAD -lt $MAXLOAD ]; then
-        # $0 is not running, so, we update the PID file
-        # and try to launch a new $PROCESS later
-        echo $CPID > $PIDF
-    else
-        EXIT=0
-        exit 0
-    fi
+    echo "We are not running?"
 fi
 
-echo -n "Starting seti at $HOSTNAME "
-if [ $LOAD -lt $MAXLOAD -a x$1 = "xstart" ]; then
-    echo "[ok]" # assume seti will work...
-    while true; do
-        # seti is smart enough to know if it's already running
-        if [ x$2 = "xlog" ]; then
-            ./$PROCESS
-        else
-            # do not show log
-            nohup ./$PROCESS > /dev/null 2>&1 &
-        fi
-        # old setiathome: -nice $NICE
-        sleep $SLEEP
-    done
+# this is indeed redundant at this point... but, just to make sure:
+# - tests whether pid file exist
+if [ is_running ]; then
+    echo -n "Starting seti at $HOSTNAME "
+    if [ $LOAD -lt $MAXLOAD -a $START ]; then
+        echo "[ok]" # seti is a loooong process. assume seti will work...
+        while true; do
+            # seti is smart enough to know if it's already running
+            if [ x$2 = "xlog" -o x$1 = "xlog" ]; then
+                ./$PROCESS
+            else
+                # do not show log
+                nohup ./$PROCESS > /dev/null 2>&1 &
+            fi
+            # old setiathome: -nice $NICE
+            sleep $SLEEP
+        done
+    else
+        echo "[failed]"
+        echo "Load $LOAD is not less than $MAXLOAD at $HOSTNAME"
+    fi
 else
-    # we should never reach here
-    echo "[failed]"
-    echo "Load $LOAD is not less than $MAXLOAD at $HOSTNAME"
+    echo "Strange error: $PIDF exist, but we are not running?"
 fi
 

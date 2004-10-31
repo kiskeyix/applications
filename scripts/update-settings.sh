79149a0adf087307162417ab222bf3f73a7bebe5
Luis Mondesi <lemsx1@gmail.com>
2004-10-31 05:27:55 +0000

    updated

--- scripts/update-settings.sh
+++ scripts/update-settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2004-Sep-28
+# Last modified: 2004-Oct-31
 #
 # DESCRIPTION: A simple script to update my settings in $HOME
 # USAGE: $0 [remove|verbose]
@@ -13,7 +13,7 @@ PATH=/bin:/usr/bin:/usr/local/bin
 WGET="`command -v wget`"
 
 URL="http://lems1.latinomixed.com"
-FILES="bashrc.tar.bz2 vimrc.tar.bz2 muttrc.tar.bz2 bin.tar.bz2"
+FILES="bashrc.tar.bz2 vimrc.tar.bz2 muttrc.tar.bz2 Applications.tar.bz2"
 VERBOSE=""
 WGET_ARGS="-c"
 REMOVE_FILES=0

4d6ed8f50e2d0e351570009efdb920cae8945730
Luis Mondesi <lemsx1@gmail.com>
2005-04-23 10:34:47 +0000

    a nice example on how to use posix threads

---
+++ doc/c_functions/threads/pthread_create.c
@@ -0,0 +1,43 @@
+#include <stdio.h>       /* standard I/O routines                 */
+#include <pthread.h>     /* pthread functions and data structures */
+
+/* function to be executed by the new thread */
+        void*
+do_loop(void* data)
+{
+        int i;			/* counter, to print numbers */
+        int j;			/* counter, for delay        */
+        int me = *((int*)data);     /* thread identifying number */
+
+        for (i=0; i<10; i++) {
+                for (j=0; j<500000; j++) /* delay loop */
+                        ;
+                printf("'%d' - Got '%d'\n", me, i);
+        }
+
+        /* exit the thread */
+        pthread_exit(NULL);
+}
+
+/* like any C program, program's execution begins in main */
+        int
+main(int argc, char* argv[])
+{
+        int        thr_id,thr_id2;         /* thread ID for the newly created thread */
+        pthread_t  p_thread,p_thread2;       /* thread's structure                     */
+        int        a         = 1;  /* thread 1 identifying number            */
+        int        b         = 2;  /* thread 2 identifying number            */
+        int         c       = 3;
+
+        /* create a new thread that will execute 'do_loop()' */
+        thr_id = pthread_create(&p_thread, NULL, do_loop, (void*)&a);
+
+        pthread_join(p_thread,NULL); /* makes it wait till thread is finished */
+        /* run 'do_loop()' in the main thread as well */
+
+        thr_id2 = pthread_create(&p_thread2, NULL, do_loop, (void*)&c);
+        /* main() is a thread by itself! */
+        do_loop((void*)&b);
+        /* NOT REACHED */
+        return 0;
+}

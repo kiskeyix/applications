de1f4eb3860049c505e02e9214c129acbc797428
Luis Mondesi <lemsx1@gmail.com>
2005-05-17 18:54:54 +0000

    added a series of notes on how to chroot sftp/ssh

--- scripts/chroot_this.pl
+++ scripts/chroot_this.pl
@@ -1,10 +1,85 @@
 #!/usr/bin/perl -w
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 # Luis Mondesi < lemsx1@gmail.com >
 #
 # DESCRIPTION: set up a chroot environment for a binary
-# USAGE: cd /chroot/path && $0 /path/to/program
+#
+# USAGE: cd /chroot/path && chroot_this.pl /path/to/program
+#
 # LICENSE: GPL
+#
+# NOTES: 
+# This script just does the right thing for the given program passed to it
+# to setup a full chroot environment you will need all the common UNIX utils 
+# used by the program you passed as an argument.
+# Here is an example of what you would need if you want to setup ssh/sftp 
+# chrooted on Fedora (with privilege separation):
+#
+# * /bin/{cp, ls, mkdir, mv, rm, rmdir, sh}: for i in cp ls mkdir mv rm rmdir sh; do chroot_this.pl /bin/$i; done
+# * /usr/libexec/sftp-server: chroot_this.pl /usr/libexec/sftp-server
+# * pam: mkdir -p etc/pam.d; cp -r /etc/pam.d/* etc/pam.d/
+# * libpam: cp -r /lib/libpam* lib
+# - cp /lib/libnss_files* lib
+# - mkdir -p lib/security; cp -r /lib/security/* lib/security
+# - mkdir -p etc/security; cp -r /etc/security/*.conf etc/security
+# * ssh: mkdir -p etc/ssh; cp -a /etc/ssh/* etc/ssh/
+# * /etc/nsswitch.conf: mkdir etc; cat > etc/nsswitch.conf <<-FIN
+#     passwd:         files
+#     group:          files
+#     shadow:         files
+#   FIN
+# * /etc/{passwd,shadow,group}:
+#   mkdir etc; cat > $CHROOTDIR/etc/passwd <<-FIN
+#        root:x:0:0:Root:/:/bin/bash
+#        nobody:x:65534:65534:nobody:/:/bin/false
+#        sshd:x:74:74:Privilege-separated SSH:/var/empty/sshd:/sbin/nologin
+#    FIN
+#    cat > etc/shadow <<-FIN
+#        root:*:11142:0:99999:7:::
+#        nobody:*:11142:0:99999:7:::
+#        sshd:x:74:74:Privilege-separated SSH:/var/empty/sshd:/sbin/nologin
+#    FIN
+#    cat > etc/group <<-FIN
+#        root:x:0
+#        nogroup:x:65534:
+#    FIN
+# * /var:
+#   - mkdir -p var/empty/sshd; chown root var/empty/sshd; chmod 0755 var/empty/sshd
+#   - mkdir -p var/log
+#   - mkdir -p var/run
+#   - mkdir -p var/tmp; chmod 1777 var/tmp
+# * /tmp: mkdir tmp; chmod 1777 tmp
+# * /etc/init.d/ssh: mkdir -p etc/init.d; cp /etc/init.d/ssh* etc/init.d/
+#   - cp /etc/init.d/functions etc/init.d/
+# * Assorted kernel devices:
+#   - mknod dev/null c 1 3
+#   - mknod dev/zero c 1 5
+#   - mknod dev/urandom c 1 9
+#   - mknod dev/console c 5 1
+#   
+# * Assorted directories:
+#   - mkdir -p etc/rc.d; cd etc/rc.d; ln -s ../init.d init.d
+#
+# * Assorted utilities:
+#   - /sbin/initlog: chroot_this.pl /sbin/initlog
+#   - /usr/bin/reset: chroot_this.pl /usr/bin/reset
+#   - /sbin/nologin: chroot_this.pl /sbin/nologin
+# * Assorted libraries:
+#   - cp -a /lib/libssl* lib/
+#   - cp -a /usr/lib/libssl* usr/lib/
+#   - cp -a /usr/lib/libcrypt* usr/lib/
+# * Assorted logging:
+#   - syslog-ng: add unix-stream(/path/to/chroot/dev/log) to your s_all source
+#   - syslog: edit /etc/sysconfig/syslog and add: -p /dev/log -a /path/to/chroot/dev/log
+# * Assorted mounts:
+#   ssh doesn't like to work without this:
+#   - /dev/pts: mkdir dev/pts; mount -t devpts none dev/pts
+#
+# And finally, run: chroot /path/to/chrootdir
+# And execute '/usr/sbin/sshd -dD' to test sshd in non-daemon debugging mode
+# From a different shell/system, use "ssh -v -v user@server_or_ip" to test the client
+# REFERENCES:
+# * http://chrootssh.sourceforge.net/docs/chrootedsftp.html
 
 use strict;
 $|++;
@@ -47,7 +122,7 @@ if ( $HELP ) {
 
 if ( $PVERSION ) { print STDOUT ($revision); exit 0; }
 
-my $usage = "From the chrooted directory do: $0 /path/to/binary";
+my $usage = "From the chrooted directory do: chroot_this.pl /path/to/binary";
 
 foreach my $bin ( @ARGV )
 {
@@ -73,7 +148,7 @@ foreach my $bin ( @ARGV )
             my $libchrootedpath = "";
             $lib =~ s/\s*\(0x[0-9a-fA-F]+\)\s*$//;              # cleaned: (0x00000da)
             my ($libname,$libpath) = split(/\s*=>\s*/,$lib);    # splitted by =>
-            if ( -r $libpath ) # can we read this library name?
+            if ( $libpath !~ /^\s*$/ and -r $libpath ) # can we read this library name?
             {
                 ($libchrootedpath = $libpath) =~ s,^/,,;            # cleaned first /
 
@@ -91,6 +166,9 @@ foreach my $bin ( @ARGV )
                 } else {
                     print STDERR "$libchrootedpath already exists\n";
                 }
+            } else {
+                print STDERR "library '$libpath' is not valid from ldd $bin\n";
+                print STDERR "try copying this file by hand or its name might simply be '$libname'; like /lib/ld-linux.so.[0-9]*\n";
             }
         }
     } else {

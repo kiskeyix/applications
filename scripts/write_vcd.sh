eb5272c27a821c23848eb91f05776170aec36713
Luis Mondesi <lemsx1@gmail.com>
2005-07-17 20:59:36 +0000

    adds eject to the picture

--- scripts/write_vcd.sh
+++ scripts/write_vcd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 # Luis Mondesi < lemsx1@hotmail.com >
 # Last modified: 2004-Aug-19
 #
@@ -12,4 +12,4 @@
 # device block:
 #DEV=/dev/dvd
 DEV=ATA:1,0,0
-cdrdao write --device $DEV --driver generic-mmc $@
+cdrdao write --device $DEV --driver generic-mmc $@ && eject

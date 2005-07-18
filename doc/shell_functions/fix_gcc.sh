308f1cdced907d54e08eb403fffd54b2e9dbcc6b
Luis Mondesi <lemsx1@gmail.com>
2005-07-18 04:36:07 +0000

    fix other apps that come with gcc

--- doc/shell_functions/fix_gcc.sh
+++ doc/shell_functions/fix_gcc.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-# $Revision: 1.4 $
+# $Revision: 1.5 $
+# $Date: 2005-07-18 04:36:07 $
 # Luis Mondesi < lemsx1@hotmail.com >
-# Last modified: 2004-May-15
 #
 # DESCRIPTION: fixes gcc symlinks when updating to a new version under debian
 # USAGE: ./fix_gcc.sh
-# CHANGELOG:
 #
 
 PRIORITY=52 # 0 - 100. higher is better
 
 GCC="/usr/bin/gcc-3.4"
 GPP="/usr/bin/g++-3.4"
+GCCBUG="/usr/bin/gccbug-3.4"
+GCOV="/usr/bin/gcov-3.4"
 
 # to remove do:
 # sudo update-alternatives --remove-all $GCC
@@ -25,5 +26,13 @@ fi
 if [ -x ${GPP} ]; then
     sudo update-alternatives --install /usr/bin/g++ g++ ${GPP} \
     $PRIORITY --slave /usr/bin/c++ c++ ${GPP}
+fi
+
+if [ -x ${GCOV} ]; then
+    sudo update-alternatives --install /usr/bin/gcov gcov ${GCOV} $PRIORITY
+fi
+
+if [ -x ${GCCBUG} ]; then
+    sudo update-alternatives --install /usr/bin/gccbug gccbug ${GCCBUG} $PRIORITY
 fi
 

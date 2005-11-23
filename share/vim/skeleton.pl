6e75c532dc087ac5ef81c705d5d6d6adfa60ea3d
Luis Mondesi <lemsx1@gmail.com>
2005-11-23 19:31:29 +0000

    adds usage. splits =pod comments better

--- share/vim/skeleton.pl
+++ share/vim/skeleton.pl
@@ -1,11 +1,23 @@
 #!/usr/bin/perl -w
-# $Revision: 1.1 $
-# $Date: 2005-09-25 22:07:24 $
+# $Revision: 1.2 $
+# $Date: 2005-11-23 19:31:29 $
 # Luis Mondesi < lemsx1@gmail.com >
 #
 # DESCRIPTION:
 # USAGE:
 # LICENSE: ___
+
+=pod
+
+=head1 NAME
+
+skeleton.pl - skeleton script for Perl
+
+=head1 DESCRIPTION 
+
+    This script ...
+
+=cut
 
 use strict;
 $|++;
@@ -37,13 +49,49 @@ use FileHandle;     # for progressbar
 # Args:
 my $PVERSION=0;
 my $HELP=0;
+my $USAGE=0;
 my $DEBUG=0;
+
+=pod
+
+=head1 SYNOPSIS
+
+B<skeleton.pl>  [-v,--version]
+                [-D,--debug] 
+                [-h,--help]
+                [-U,--usage]
+
+=head1 OPTIONS
+
+=over 8
+
+=item -v,--version
+
+Prints version and exits
+
+=item -D,--debug
+
+Enables debug mode
+
+=item -h,--help
+
+Prints this help and exits
+
+=item -U,--usage
+
+Prints usage information and exits
+
+=back
+
+=cut
+
 # get options
 GetOptions(
     # flags
     'v|version'         =>  \$PVERSION,
     'h|help'            =>  \$HELP,
     'D|debug'           =>  \$DEBUG,
+    'U|usage'           =>  \$USAGE,
     # strings
     #'o|option=s'       =>  \$NEW_OPTION,
     # numbers
@@ -57,45 +105,19 @@ if ( $HELP ) {
     exit 0;
 }
 
+if ( $USAGE ) { 
+    use Pod::Usage;
+    pod2usage(1);
+    exit 0; # never reaches here
+}
+
 if ( $PVERSION ) { print STDOUT ($revision); exit 0; }
 
 print "sample";
 
-__END__
+=pod
 
-=head1 NAME
-
-skeleton.pl - skeleton script for Perl
-
-=head1 SYNOPSIS
-
-B<skeleton.pl>  [-v,--version]
-                [-D,--debug] 
-                [-h,--help]
-
-=head1 DESCRIPTION 
-
-    This script ...
-
-=head1 OPTIONS
-
-=over 8
-
-=item -v,--version
-
-prints version and exits
-
-=item -D,--debug
-
-enables debug mode
-
-=item -h,--help
-
-prints this help and exits
-
-=back
-
-=head1 AUTHOR
+=head1 AUTHORS
 
 Luis Mondesi <lemsx1@gmail.com>
 

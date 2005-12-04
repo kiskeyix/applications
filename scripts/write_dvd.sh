d1c008c72c00f02c217545ffab040846a02365f1
Luis Mondesi <lemsx1@gmail.com>
2005-12-04 20:47:19 +0000

    adds support for writing a dvdauthor directory

--- scripts/write_dvd.sh
+++ scripts/write_dvd.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 # Luis Mondesi < lemsx1@gmail.com >
 #
 # DESCRIPTION: A simple script to burn a .iso DVD Video to the drive
-# USAGE: write_dvd.sh dvd.iso
+# USAGE: write_dvd.sh {/path/to/dvd.iso|/path/to/dir}
 # CHANGELOG:
 # LICENSE: GPL
 
 if [ -f $1 ]; then
     growisofs -dvd-compat -Z /dev/dvd=$1 -dvd-video
+elif [ -d $1 ]; then
+    growisofs -dvd-compat -Z /dev/dvd -dvd-video $1
 else 
-    echo "Usage: write_dvd.sh dvd.iso"
+    echo "Usage: write_dvd.sh {dvd.iso|directory}"
 fi

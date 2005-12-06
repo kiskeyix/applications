50ede5376b3de71a6123f9b647d390252d18d59b
Luis Mondesi <lemsx1@gmail.com>
2005-12-06 14:57:57 +0000

    move arguments around, though that should not make a difference

--- scripts/write_dvd.sh
+++ scripts/write_dvd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 # Luis Mondesi < lemsx1@gmail.com >
 #
 # DESCRIPTION: A simple script to burn a .iso DVD Video to the drive
@@ -8,9 +8,9 @@
 # LICENSE: GPL
 
 if [ -f $1 ]; then
-    growisofs -dvd-compat -udf -Z /dev/dvd=$1 -dvd-video
+    growisofs -dvd-video -udf -dvd-compat -Z /dev/dvd=$1
 elif [ -d $1 ]; then
-    growisofs -dvd-compat -udf -Z /dev/dvd -dvd-video $1
+    growisofs -dvd-video -udf -dvd-compat -Z /dev/dvd $1
 else 
     echo "Usage: write_dvd.sh {dvd.iso|directory}"
 fi

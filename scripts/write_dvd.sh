95fff01d95a131f9712eac0a992980ddf92011e8
Luis Mondesi <lemsx1@gmail.com>
2005-12-12 12:30:38 +0000

    we do not call mkisofs when using an iso

--- scripts/write_dvd.sh
+++ scripts/write_dvd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Revision: 1.4 $
+# $Revision: 1.5 $
 # Luis Mondesi < lemsx1@gmail.com >
 #
 # DESCRIPTION: A simple script to burn a .iso DVD Video to the drive
@@ -8,7 +8,7 @@
 # LICENSE: GPL
 
 if [ -f $1 ]; then
-    growisofs -dvd-video -udf -dvd-compat -Z /dev/dvd=$1
+    growisofs -dvd-compat -Z "/dev/dvd=$1"
 elif [ -d $1 ]; then
     growisofs -dvd-video -udf -dvd-compat -Z /dev/dvd $1
 else 

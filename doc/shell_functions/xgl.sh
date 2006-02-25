7dd6c448f52402f1775647fed954a1520691b643
Luis Mondesi <lemsx1@gmail.com>
2006-02-25 15:51:21 +0000

    modified a bit...

--- doc/shell_functions/xgl.sh
+++ doc/shell_functions/xgl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 # Luis Mondesi < lemsx1@gmail.com >
 #
 # DESCRIPTION: A script to start Xgl and a gnome session with Compiz+gnome-window-decorator
@@ -14,18 +14,25 @@ MY_DISPLAY=:1
 
 Xgl $MY_DISPLAY -ac -accel xv -accel glx:pbuffer -fullscreen &
 if [ $? == 0 ]; then
+    sleep 6
+    
     DISPLAY=$MY_DISPLAY
     export DISPLAY
+
     echo "Now run: xterm -display $DISPLAY"
-    echo "Switch to that xterm in tty7 and run: compiz --replace gconf"
+    echo "Switch to that xterm in tty7 and run:"
     echo "gnome-window-decorator"
+    echo "compiz --replace gconf"
     echo "CTRL+Z to background and: bg"
     echo "exec ssh-agent dbus-launch --exit-with-session /usr/bin/gnome-session"
     echo "Trying to execute it for you..."
+    
+    gnome-window-decorator &
+    sleep 1
+    compiz --replace gconf # no need to background this. it will do it alone
+    sleep 1
     xterm -display $DISPLAY &
-    xterm -display $DISPLAY -e "compiz --replace gconf" # no need to background this
-    sleep 3
-    xterm -display $DISPLAY -e "gnome-window-decorator &" &
+    sleep 1
     #xterm -display $DISPLAY -e "exec ssh-agent dbus-launch --exit-with-session /usr/bin/gnome-session" &
     exec ssh-agent dbus-launch --exit-with-session /usr/bin/gnome-session
 else

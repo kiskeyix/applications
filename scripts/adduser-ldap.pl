023a5e66b8c8c87237d5c86c2e5a36d1153744e1
Luis Mondesi <lemsx1@gmail.com>
2006-08-25 06:44:22 +0000

    a lot of improvements

--- scripts/adduser-ldap.pl
+++ scripts/adduser-ldap.pl
@@ -1,24 +1,34 @@
 #!/usr/bin/perl -w
-# $Revision: 1.2 $
-#
+# $Revision: 1.3 $
+# 
 # Adds user to ldap server running on ldap://localhost:389
+#
+# prints LDIF if --ldif is passed
 #
 use strict;
 $|++; # disable buffer (autoflush)
+
+use Net::LDAP;
+use URI;
 use Getopt::Long;
 Getopt::Long::Configure('bundling');
 
-my $USAGE = 'adduser-ldap.pl --uid="username" --first="First" --last="Lastname" [--mailid="mail_id"] [--domain="domain.com"]';
+my $USAGE = 'adduser-ldap.pl [--uid="username"] <--first="First"> <--last="Lastname"> [--password="passwd"] [--email="mail_id"] <--domain="domain.com"> [--organizational-unit|--ou="People"] [--posix] [--nt]';
 
-my $pass_cmd = "slappasswd";
+#my $pass_cmd = "slappasswd";
 my $pass_scheme = "\{MD5\}"; # SSHA, SHA1, MD5, CRYPT
 
-my $search_cmd = "ldapsearch -x -Z localhost ";
+#my $search_cmd = "ldapsearch -x -Z localhost ";
 
 my $min_uid = "2000";
 my $max_uid = "65000";
 
 my $HELP;
+my $DEBUG;
+my $POSIX;
+my $NT;
+my $OU;
+my $LDIF;
 
 my %rec = ();
 
@@ -39,49 +49,69 @@ $rec{'title'} = "";
 $rec{'passwd'} = "personal";
 
 GetOptions(
+    'debug'             =>\$DEBUG,
+    'posix'             =>\$POSIX,
+    'nt'                =>\$NT,
     'f|first=s'         =>\$first,
     'l|last=s'          =>\$last,
     'u|uid=s'           =>\$uid,
-    'm|mailid=s'        =>\$mid,
+    'e|m|email=s'       =>\$mid,
     'd|domain=s'        =>\$domain,
-    'p|passwd=s'        =>\$passwd,
+    'o|ou|organizational-unit=s'    =>\$OU,
+    'p|password=s'      =>\$passwd,
+    'ldif'              =>\$LDIF,
     'h|help'            =>\$HELP
 );
 
 if ( $HELP ) { print $USAGE; exit 0; }
 
-die ($USAGE) if ($first eq "" || $last eq "" || $domain eq "");
-
-# TODO if interactive, prompt for missing $rec:
+print STDERR ($USAGE) and exit(1)
+    if ($first eq "" || $last eq "" || $domain eq "");
 
 # set some internal vars:
-my ($fq,$dn) = split(/\./,$domain);
-$passwd = qx/$pass_cmd -h $pass_scheme -s $rec{'passwd'}/ if ($passwd eq "");
+my @domain_parts = split(/\./,$domain);
+
+# TODO emulate slapasswd
+#$passwd = qx/$pass_cmd -h $pass_scheme -s $rec{'passwd'}/ if ($passwd eq "");
 
 # create UID and MID using scheme: 
 #   (first letter of first name) + (last name)
-$uid = ( $uid ne "" ) ? $uid : substr ( $first, 0, 1 ).$last;
-$mid = ( $mid ne "" ) ? $mid : substr ( $first, 0, 1 ).$last;
+$uid = ( $uid ne "" ) ? $uid : lc(substr ( $first, 0, 1 ).$last);
+$mid = ( $mid ne "" ) ? $mid : lc(substr ( $first, 0, 1 ).$last);
 
-my $home = "/home/$uid";
+# TODO --home-path
+my $home = "/home/$uid"; # --posix only
 
 my $full_name = ucfirst($first)." ".ucfirst($last);
 my $initials = substr($first,0,1).substr($last,0,1);
 
+# --posix only 
 # TODO calculate uid number from previous LDAP user_list
 my $uid_number = sprintf ("%02d",rand(my @ary = ($min_uid .. $max_uid)));
 my $gid_number = "100"; # TODO get a good GID number from LDAP
 
 # TODO sanity checks: mid, uid, uidNumber, gidNumber, ...
 
+my $ou = ( $OU ) ? "ou=$OU, " : "";
+my $domain_joined = "";
+foreach(@domain_parts)
+{
+    $domain_joined .= "dc=$_, "
+}
+$domain_joined =~ s/, $//;
+
 my $ldif = "
-dn: cn=$full_name, dc=$fq, dc=$dn
+dn: cn=$full_name, $ou $domain_joined
 objectClass: top
 objectClass: person
-objectClass: posixAccount
-objectClass: shadowAccount
 objectClass: organizationalPerson
-objectClass: inetOrgPerson
+objectClass: inetOrgPerson";
+
+$ldif .= "
+objectClass: posixAccount
+objectClass: shadowAccount" if ($POSIX);
+
+$ldif .= "
 cn: $full_name
 sn: $last
 givenName: $first
@@ -98,7 +128,9 @@ departmentNumber:
 employeeNumber:
 employeeType: full time
 preferredLanguage: en
-labeledURI: http://$rec{'webhost'}.$domain/~$uid User Home Page
+labeledURI: http://$rec{'webhost'}.$domain/~$uid User Home Page"; 
+
+$ldif .= "
 homeDirectory: $home
 gecos: $full_name
 shadowMin: -1
@@ -110,6 +142,11 @@ uidNumber: $uid_number
 gidNumber: $gid_number
 userpassword: $passwd
 
-";
+" if ($POSIX);
 
-print STDOUT $ldif;
+if ($LDIF or $DEBUG)
+{
+    print STDOUT ($ldif,"\n");
+}
+
+# connect to LDAP server and do your thing

127e8d05a0f136241bce650f62cd667a00e55bbe
Luis Mondesi <lemsx1@gmail.com>
2007-05-24 06:41:31 +0000

    adds comment as to why there cannot be 2 modules with the same name while the variable "our" is in used. they should be changed to "my" to make it local to their own scope.

--- share/vim/skeleton.pm
+++ share/vim/skeleton.pm
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# $Revision: 1.10 $
+# $Revision: 1.11 $
 # my_name < email@example.com >
 #
 # DESCRIPTION: A simple package that exports ...
@@ -10,6 +10,18 @@
 #               - options are configured with setters/getters
 #                 for our configurable properties
 # LICENSE: GPL
+# NOTES:
+#  * The reason behind using "our" for @ISA, %EXPORT_TAGS, 
+#    and @EXPORT_OK is that it makes things simpler when inheriting
+#    methods/variables from base classes. All you need to do really
+#    is add the module names to 'our @ISA = qw()' and all the methods
+#    exported by that module will be accessible.
+#    This creates the problem, then, that if you have 2 classes created
+#    by this file with the set_option/get_option (which have the same
+#    name), these will be seen by Perl as "redefined" and an error will
+#    be croak'd. To fix this glitch simply substitute "our" with "my"
+#    to make the scope of those methods local to their own objects.
+#
 
 =pod
 

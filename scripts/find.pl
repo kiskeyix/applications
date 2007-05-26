bf505395d76595a54eb5195580ef7b75c6943984
Luis Mondesi <lemsx1@gmail.com>
2007-05-26 05:37:15 +0000

    optimized code

--- scripts/find.pl
+++ scripts/find.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# $Revision: 1.30 $
+# $Revision: 1.31 $
 # Luis Mondesi < lemsx1@gmail.com >
 #
 # URL: http://www.kiskeyix.org/downloads/find.pl.gz
@@ -16,9 +16,8 @@
 #
 # LICENSE: GPL
 
-
-use File::Find;     # find();
-use File::Basename; # basename();
+use File::Find;        # find();
+use File::Basename;    # basename();
 use Getopt::Long;
 Getopt::Long::Configure('bundling');
 
@@ -31,180 +30,173 @@ my $EXCEPTION_LIST = "\.soc\$|\.sock\$|\.so\$|\.o\$|\.swp\$";
 
 # some colors:
 
-my $RED = "\033[1;31m";
-my $NORM = "\033[0;39m";
+my $RED   = "\033[1;31m";
+my $NORM  = "\033[0;39m";
 my $GREEN = "\033[0;32m";
-my $BLUE = "";
+my $BLUE  = "\033[0;34m";
 
 # -------------------------------------------------------------------
 #           NO NEED TO MODIFY ANYTHING PASS THIS LINE               #
 # -------------------------------------------------------------------
 
-my $usage = "Usage: find.pl [--replace=\"string\"] \"string\" [\"FILE_REGEX\"]\n NOTE use quotes to avoid the shell expanding your REGEX";
-my $modified = 0;
+my $usage =
+  "Usage:\nfind.pl [-f,--file-pattern=\"REGEX\"] [-r,--replace=\"replacement\"] <-s,--string=\"string\">\nfind.pl <\"string to find\"> [\"REGEX file pattern\"] [\"replacement string\"]\n\nNOTE use quotes to avoid the shell expanding your REGEX\n\n";
 
-my @new_file = ();      # lines to be printed in new file
-my @ls = ();            # array of files
-
-my $this_string=undef;
-my $that_string=undef;
-my $f_pattern = undef;
+my $this_string = undef;
+my $that_string = undef;
+my $f_pattern   = undef;
 
 GetOptions(
-    # flags
-    #'v|version'         =>  \$PVERSION,
-    #'h|help'            =>  \$HELP,
-    'D|debug'           =>  \$DEBUG,
-    # strings
-    'r|replace=s'      =>   \$that_string
-) and $this_string = shift and $f_pattern = shift;
 
-die ( "Sorry. Can't search for (nul)" ) 
-    if ( defined($this_string) and $this_string =~ m/^\s*$/ );
+    'D|debug'          => \$DEBUG,
+    'f|file-pattern=s' => \$f_pattern,
+    's|string=s'       => \$this_string,
+    'r|replace=s'      => \$that_string
+  )
+  and (not defined $this_string and $this_string = shift)
+  and (not defined $f_pattern   and $f_pattern   = shift)
+  and (not defined $that_string and $that_string = shift);
 
-if ( defined($f_pattern) and $f_pattern =~ m#^[^\\]\.# )
+print STDERR ($usage) and die("Sorry. Can't search for (nul)\n")
+  if (not defined($this_string) or $this_string =~ m/^\s*$/);
+
+if (defined($f_pattern) and $f_pattern =~ m#^[^\\]\.#)
 {
-    print STDERR "WARNING: using a dot in file pattern can match too many files. Escape dots with '\.'.\n Waiting 5 seconds before continuing\n Press CTRL+C to abort script execution\n" ;
+    print STDERR
+      "WARNING: using a dot in file pattern can match too many files. Escape dots with '\.'.\n Waiting 5 seconds before continuing\n Press CTRL+C to abort script execution\n";
     sleep(5);
 }
 
-if ( not defined($f_pattern) ) {
+if (not defined($f_pattern))
+{
     print STDERR "All files chosen\n";
     $f_pattern = ".*";
 }
 
-$that_string = clean_string($that_string ) if ( defined($that_string) );
-$this_string = clean_string($this_string );
+$that_string = _clean_string($that_string) if (defined($that_string));
+$this_string = _clean_string($this_string);
 
-if ( $DEBUG > 0 )
+if ($DEBUG > 0)
 {
     print STDERR "search for: '$this_string' \n";
     print STDERR "replace with: '$that_string' \n" if (defined($that_string));
-    print STDERR "file pattern: '$f_pattern' \n" if (defined($f_pattern));
+    print STDERR "file pattern: '$f_pattern' \n"   if (defined($f_pattern));
     print STDERR "\n";
     print STDERR "$RED DEBUG in place... pausing for 5 seconds$NORM\n";
     sleep(5);
 }
 
+# main()
 if ($this_string =~ /[[:alnum:]\.\_\-\(\)\[\]\{\}\"\']+/)
 {
-    my $i=0;
-    @ls = do_file_ary("."); # start at current directory
-    print STDERR ("File list: ",join(":",@ls),"\n") if ( $DEBUG ); 
-    foreach my $_file (@ls)
-    {
-        # yes, this is a wrapper for a standard tip!
-        #
-        # open e/a file if it's a regular file
-        # and replace $this_string with $that_string
-        # if $that_string is set
-        # and keep a backup .bak for e/a file modified
-        #system("perl -e 'm/$this_string/g;' $_");
-        # or 
-        #system("perl -e 's/$this_string/$that_string/g;' -pi.bak $_");
-
-        if ( $DEBUG ) { print STDERR "opening $_file\n"; }
-
-        $i = 0;
-        $modified = 0; # clear flag
-
-        open (FILE,"<$_file") or die "could not open $_file. $!\n";
-        if ( defined($that_string) )
-        {
-            while(<FILE>) {
-                $i++;
-                if ($_ =~ s|$this_string|$that_string|g) {
-                    print STDOUT "$GREEN $_file [$i]:$NORM $_"; 
-                    $modified = 1;
-                }
-                push @new_file,$_;
-            }
-            close(FILE);
-            if ($modified) {
-                open (NEWFILE,">$_file") 
-                    or die "could not write to $_file. $!\n";
-                print NEWFILE @new_file;
-                close(NEWFILE);
-            }
-            # cleanup array
-            @new_file = ();
-        } else {
-            while(<FILE>) { 
-                $i++; 
-                if ($_ =~ m|$this_string|gi) {
-                    print STDOUT "$_file [$i]: $_"; 
-                }
-            }
-            close(FILE);
-        }
-    } #end for
-} else {
-    print STDERR ($usage,"\n");
+    _process_files(".");
 }
+else
+{
+    print STDERR ($usage);
+    exit 1;
+}
+exit 0;
 
-sub clean_string
+# supporting functions
+
+sub _clean_string
 {
     my $str = shift;
-    return undef if ( not defined $str );
+    return undef if (not defined $str);
 
-    $str =~ s,\|,\\|,g; # take care of |
-    $str =~ s/\\$//g; # take care of ending \
+    $str =~ s,\|,\\|,g;    # take care of |
+    $str =~ s/\\$//g;      # take care of ending \
 
     return $str;
 }
 
-sub do_file_ary {
+sub _process_files
+{
+
     # uses find() to recur thru directories
-    # returns an array of files
-    # i.e. in directory "a" with the files:
-    # /a/file.txt
-    # /a/b/file-b.txt
-    # /a/b/c/file-c.txt
-    # /a/b2/c2/file-c2.txt
-    # 
-    # my @ary = &do_file_ary(".");
-    # 
-    # will yield:
-    # a/file.txt
-    # a/b/file-b.txt
-    # a/b/c/file-c.txt
-    # a/b2/c2/file-c2.txt
-    # 
     my $ROOT = shift;
-    
-    my %opt = (wanted => \&process_file, no_chdir=>1);
-    
-    find(\%opt,$ROOT);
-    
-    return @ls;
+
+    my %opt = (wanted => \&_process_file, no_chdir => 1);
+
+    find(\%opt, $ROOT);
 }
 
-sub is_binary
+sub _is_binary
 {
+
     # returns 1 if true
     my $file = shift;
-    print STDERR ( "is_binary() called: $file\n" ) if ( $DEBUG );
-    my $file_t = qx/file "$file"/;
-    if ( $file_t =~ m/(text\s+executable|\btext\b)/i )
+    print STDERR ("is_binary() called: $file\n") if ($DEBUG);
+    my $file_t = qx/file "$file"/; # FIXME very expensive call. re-write in Perl
+    if ($file_t =~ m/(text\s+executable|\btext\b)/i)
     {
         return 0;
     }
-    print STDERR ( "binary file\n" ) if ( $DEBUG );
+    print STDERR ("binary file\n") if ($DEBUG);
     return 1;
 }
 
-sub process_file {
-    print STDERR ("Processing : ",$_,"\n") if ( $DEBUG );
-    my $base_name = basename($_);
-    if ( 
-        $_ =~ m($f_pattern) and
-        -f $_ and
-        $base_name !~ m($EXCEPTION_LIST) and
-        ! is_binary ($_)
-    ) {
-        s/^\.\/*//g;
-        print STDERR ("Pushing : ",$_,"\n") if ( $DEBUG );
-        push @ls,$_;
+sub _process_file
+{
+    print STDERR ("Processing : ", $_, "\n") if ($DEBUG);
+    if (    $_ =~ m($f_pattern)
+        and -r $_
+        and basename($_) !~ m($EXCEPTION_LIST)
+        and !_is_binary($_))
+    {
+        my $_file = $_;
+        print STDERR ("opening $_file\n") if ($DEBUG);
+        my $_file_tmp = "$_file.$$.tmp";
+
+        my $modified = 0;
+        my $i        = 0;
+
+        if (-f $_file_tmp)
+        {
+            unlink($_file_tmp)
+              or die("Could not remove file $_file_tmp. $!\n");
+        }
+
+        open(FILE, "<$_file") or die "could not open $_file. $!\n";
+
+        while (<FILE>)
+        {
+            $i++;
+            if (defined($that_string))
+            {
+                if ($_ =~ s|$this_string|$that_string|g)
+                {
+                    print STDOUT "$GREEN $_file [$i]:$NORM $_";
+                    $modified = 1;
+                }
+                open(NEWFILE, ">>$_file_tmp")
+                  or die("could not write to $_file_tmp. $!\n");
+                print NEWFILE $_;
+                close(NEWFILE);
+            }
+            else
+            {
+                if ($_ =~ m|$this_string|gi)
+                {
+                    print STDOUT "$BLUE $_file [$i]:$NORM $_";
+                }
+            }
+        }
+        close(FILE);
+        if ($modified)
+        {
+            if (-r "$_file_tmp")
+            {
+                rename("$_file_tmp", $_file);
+            }
+            else
+            {
+                die("Cannot read file $_file_tmp eventhough we created it? $!\n"
+                );
+            }
+            $modified = 0;    # just in case...
+        }
     }
 }
 

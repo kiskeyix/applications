9df176bd3db3a6ced9085a39e16ec5cfecc7f8ba
Luis Mondesi <lemsx1@gmail.com>
2007-06-06 03:10:35 +0000

    a simple C++ file that prints sample

---
+++ share/vim/skeleton.cc
@@ -0,0 +1,19 @@
+/*
+ * $Revision: 1.1 $
+ * $Date: 2007-06-06 03:10:35 $
+ * vi: ft=cpp :
+ * my_name < email@example.com >
+ *
+ * DESCRIPTION:
+ * USAGE:
+ * LICENSE: ___
+ */
+#include <iostream>              /* cout */
+using namespace std;
+
+int
+main (int argc, char **argv)
+{
+        cout << "sample" << endl;
+        return 0;
+}

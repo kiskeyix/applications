4d280872fbb174a3b91a75d77db05da83b2d9866
Luis Mondesi <lemsx1@gmail.com>
2007-06-06 03:08:01 +0000

    new C skeleton

---
+++ share/vim/skeleton.c
@@ -0,0 +1,18 @@
+/*
+ * $Revision: 1.1 $
+ * $Date: 2007-06-06 03:08:01 $
+ * vi: ft=c :
+ * my_name < email@example.com >
+ *
+ * DESCRIPTION:
+ * USAGE:
+ * LICENSE: ___
+ */
+#include <stdio.h>              /* printf */
+
+int
+main (int argc, char **argv)
+{
+        printf ("sample");
+        return 0;
+}

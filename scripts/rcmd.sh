cfee67f368c6d61553f706c0dd776b02943b6869
Luis Mondesi <lemsx1@gmail.com>
2007-10-19 09:44:42 -0400

    modernized
    GPL'd and code cleanups

--- scripts/rcmd.sh
+++ scripts/rcmd.sh
@@ -1,38 +1,30 @@
 #!/bin/sh
+# $Revision: 1.3 $
+# $Date: 2007-03-01 21:41:46 $
+# Luis Mondesi < lemsx1@gmail.com >
 #
-# Luis Mondesi < lemsx1@hotmail.com >
-#
+# DESCRIPTION:
 # Script to distribute commands to 
 # an array of servers. Useful for
 # clusters and maintaining lots of servers
-#
+# USAGE: $0 <cmd> [args]
+# LICENSE: GPL
 # FILES: ~/.remote-hosts    # user's remote hosts. formatted like /etc/hosts (man 5 hosts)
-#
 
 # TODO add hostname ( \2 )
 HOSTS="`sed 's/^\([0-9\.]\+[0-9]\+\).*/\1/' ~/.remote-hosts`"
-RSH=ssh
+RSH="ssh"
 
 R_CMD="$@"
-
-OLD_IFS="$IFS"
 
 #echo "hosts: $HOSTS"
 
 if [ "x$1" != "x" ]; then
-    #IFS=";"
+    # at least one command was given
     for i in $HOSTS; do
-        # IFS=""
-        #set -- `echo $i`
-        #IP="$1.$2.$3.$4"
-        #echo "IP: $i []"
-        #HOST=$@
         echo "$RSH $i $R_CMD"
         $RSH $i $@
-        #done
     done
 else
-    echo -e "Usage: $0 CMD \n \t Where CMD is any command \n Put your remote hosts names/ips (one per line) in ~/.remote-hosts (man 5 hosts)"
+    printf "Usage: $0 <CMD> [args] \n \t Where CMD is any command \n Put your remote hosts names/ips (one per line) in ~/.remote-hosts (man 5 hosts)"
 fi
-
-IFS=$OLD_IFS

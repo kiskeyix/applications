6c64a0e0bdf7e13720c0d02a76e9c51921a68fae
Luis Mondesi <lmondesi@fotolog.com>
2010-02-01 15:17:58 -0500

    adds debug and verbose. useful vars

--- share/vim/skeleton.rb
+++ share/vim/skeleton.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 # == Synopsis
 #
 # skeleton: foo bar
@@ -39,18 +39,27 @@ opts = GetoptLong.new(
 [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
 [ '--usage', '-U', '-?', GetoptLong::NO_ARGUMENT ],
 [ '--repeat', '-n', GetoptLong::REQUIRED_ARGUMENT ],
-[ '--name', GetoptLong::OPTIONAL_ARGUMENT ]
+[ '--name', GetoptLong::OPTIONAL_ARGUMENT ],
+[ '--verbose', '-v', GetoptLong::NO_ARGUMENT ],
+[ '--debug', '-D', GetoptLong::NO_ARGUMENT ]
 )
 
-dir = nil
-name = nil
+dir         = nil
+name        = nil
 repetitions = 1
+$_verbose   = false
+$_debug     = false
+
 opts.each do |opt, arg|
    case opt
    when '--help'
       RDoc::usage
    when '--usage'
       RDoc::usage
+   when '--verbose'
+      $_verbose=true
+   when '--debug'
+      $_debug=true
    when '--repeat'
       if arg.chomp.empty? || arg.to_i < 1
          puts 'Repeat number is wrong (try --help)'
@@ -68,14 +77,43 @@ opts.each do |opt, arg|
    end
 end
 
+# helpers
+def scolor(msg,color)
+   colors = {
+      'red'    => "\033[1;31m",
+      'norm'   => "\033[0;39m",
+      'green'  => "\033[0;32m",
+      'blue'   => "\033[0;34m"
+   }
+   ansicolor = "#{colors[color.downcase]}#{msg}#{colors['norm']}"
+end
+def debug(msg,val="")
+   return if not $_debug
+  
+   $stderr.print scolor("DEBUG: ",'green')
+   if val
+      # val.to_s is called for us:
+      $stderr.puts "#{scolor(msg,'blue')} = #{scolor(val,'red')}"
+   else
+      $stderr.puts "#{scolor(msg,'blue')}"
+   end
+end
+def verbose(msg)
+   return if not $_verbose
+   puts "#{msg}"
+end
+# end helpers
+
+# main()
+
 # if ARGV.length != 1
 #    puts "Missing dir argument (try --help)"
 #    RDoc::usage 1
 #    exit 1 # never reaches here
 # end
-# 
+#
 # dir = ARGV.shift
-# 
+#
 # Dir.chdir(dir)
 # for i in (1..repetitions)
 #    print "Hello"
@@ -85,4 +123,9 @@ end
 #    puts
 # end
 
-puts "Hello"
+str = "Hello"
+val = "World"
+
+debug(str,val)
+verbose("printing all variables: ")
+puts str + " " + val
